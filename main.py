012afffbec80663a2d34cfc7d7647704c934ee72
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-15 10:49:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from firebase_setup import auth
-from content_page import content_page
+from firebase_setup import initialize_firebase
 from login_page import login_page
+from content_page import content_page
 
 def main():
     st.set_page_config(page_title="App com Streamlit e Firebase")
-    
-    user = login_page()
+    auth = initialize_firebase()
+    user = login_page(auth)
     content_page(user)
 
 if __name__ == "__main__":
