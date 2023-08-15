3d01c84da7095bdccfeeb782fb5939ab0db8c547
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-15 11:14:40 -0300

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -7,8 +7,11 @@ def initialize_firebase():
     with open("firebase_config.json") as json_file:
         firebase_config = json.load(json_file)
     
+    print("Firebase Config:", firebase_config)  # Adicione esta linha para depuração
+    
     # Inicializa o Firebase
     firebase = pyrebase.initialize_app(firebase_config)
     
     # Retorna a instância de autenticação do Firebase
     return firebase.auth()
+
