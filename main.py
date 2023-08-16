dd110f6b6f3ee8261900d1eddcc2f5dcf4e73b01
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-16 15:01:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,31 +23,10 @@ class MultiApp:
         })
 
     def run():
-        # app = st.sidebar(
-        with st.sidebar:        
-            app = option_menu(
-                menu_title='Pondering ',
-                options=['Account','Trending','Your Posts','about'],
-                icons=['house-fill','person-circle','trophy-fill','chat-fill','info-circle-fill'],
-                menu_icon='chat-text-fill',
-                default_index=1,
-                styles={
-                    "container": {"padding": "5!important","background-color":'black'},
-        "icon": {"color": "white", "font-size": "23px"}, 
-        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
-        "nav-link-selected": {"background-color": "#02ab21"},}
-                
-                )
-
-        
+           
         if app == "Account":
             test.app()    
-        if app == "Trending":
-            trending.app()        
-        if app == 'Your Posts':
-            your.app()
-        if app == 'about':
-            about.app()    
+       
              
           
              
