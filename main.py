949220c356fe8bce658b8d67038752fdb8a9aebf
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-16 15:43:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,45 +8,46 @@ st.set_page_config(
         page_title="Football Data Analysis",
 )
 
+
+
 class MultiApp:
 
     def __init__(self):
         self.apps = []
 
     def add_app(self, title, func):
+
         self.apps.append({
             "title": title,
             "function": func
         })
 
-def run():
-    multi_app = MultiApp()
-    multi_app.add_app("Conta", login.app)
-    multi_app.add_app("CS", cs.app)
-    
+    def run():
+        # app = st.sidebar(
+        with st.sidebar:        
+            app = option_menu(
+                menu_title='Football Data Analysis ',
+                options=['Conta','CS'],
+                icons=['house-fill','person-circle','trophy-fill','chat-fill','info-circle-fill'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important","background-color":'black'},
+        "icon": {"color": "white", "font-size": "23px"}, 
+        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
+        "nav-link-selected": {"background-color": "#02ab21"},}
+                
+                )
 
-    with st.sidebar:
-        app = option_menu(
-            menu_title='FDA ',
-            options=['Conta', 'CS'],
-            icons=['house-fill', 'person-circle', 'trophy-fill', 'chat-fill', 'info-circle-fill'],
-            menu_icon='chat-text-fill',
-            default_index=1,
-            styles={
-                "container": {"padding": "5!important", "background-color": 'black'},
-                "icon": {"color": "white", "font-size": "23px"},
-                "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "blue"},
-                "nav-link-selected": {"background-color": "#02ab21"},
-            }
-        )
-
-    if app == "Conta":
-        login.app()
-    if app == "CS":
-        cs.app()
-   
-
-run()
-
+        
+        if app == "Conta":
+            login.app()
+        if app == "CS":
+            cs.app()    
+             
+          
+             
+    run()            
+         
 
 
