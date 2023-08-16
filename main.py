261c013bcce0efaecd1dfa29990d18853d6d288e
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-16 16:12:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,54 @@
 import streamlit as st
-
 from streamlit_option_menu import option_menu
+from login import app as login_app
+from cs import app as cs_app
 
-
-import login, cs
 st.set_page_config(
-        page_title="Football Data Analysis",
+    page_title="Football Data Analysis",
 )
 
-
-
 class MultiApp:
-
     def __init__(self):
         self.apps = []
 
     def add_app(self, title, func):
-
         self.apps.append({
             "title": title,
             "function": func
         })
 
+    @staticmethod
     def run():
-        # app = st.sidebar(
-        with st.sidebar:        
+        with st.sidebar:
             app = option_menu(
                 menu_title='Football Data Analysis ',
-                options=['Conta','CS'],
-                icons=['house-fill','person-circle','trophy-fill','chat-fill','info-circle-fill'],
+                options=['Conta', 'CS'],
+                icons=['house-fill', 'person-circle', 'trophy-fill', 'chat-fill', 'info-circle-fill'],
                 menu_icon='chat-text-fill',
                 default_index=1,
                 styles={
-                    "container": {"padding": "5!important","background-color":'black'},
-        "icon": {"color": "white", "font-size": "23px"}, 
-        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
-        "nav-link-selected": {"background-color": "#02ab21"},}
-                
-                )
+                    "container": {"padding": "5!important", "background-color": 'black'},
+                    "icon": {"color": "white", "font-size": "23px"},
+                    "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "blue"},
+                    "nav-link-selected": {"background-color": "#02ab21"},
+                }
+            )
 
-        
         if app == "Conta":
-            login.app()
+            login_app()
         if app == "CS":
-            cs.app()    
-             
-          
-             
-    run()            
+            cs_app()
+
+# Criar uma instância de MultiApp
+multi_app = MultiApp()
+
+# Adicionar os aplicativos à instância
+multi_app.add_app("Conta", login_app)
+multi_app.add_app("CS", cs_app)
+
+# Executar o aplicativo selecionado
+multi_app.run()
+
          
 
 
