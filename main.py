2415810fd2b0d189e09a0cb83a0b1400a8ec3b1e
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-16 15:23:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import streamlit as st
-
 from streamlit_option_menu import option_menu
-
-
 import login, cs, tips
+
 st.set_page_config(
-        page_title="Football Data Analysis",
+    page_title="Football Data Analysis",
 )
-
-
 
 class MultiApp:
 
@@ -16,38 +12,40 @@ class MultiApp:
         self.apps = []
 
     def add_app(self, title, func):
-
         self.apps.append({
             "title": title,
             "function": func
         })
 
-    def run():
-        # app = st.sidebar(
-        with st.sidebar:        
-            app = option_menu(
-                menu_title='FDA ',
-                options=['Conta','CS','Tips'],
-                icons=['house-fill','person-circle','trophy-fill','chat-fill','info-circle-fill'],
-                menu_icon='chat-text-fill',
-                default_index=1,
-                styles={
-                    "container": {"padding": "5!important","background-color":'black'},
-        "icon": {"color": "white", "font-size": "23px"}, 
-        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
-        "nav-link-selected": {"background-color": "#02ab21"},}
-                
-                )
+def run():
+    multi_app = MultiApp()
+    multi_app.add_app("Conta", login.app)
+    multi_app.add_app("CS", cs.app)
+    multi_app.add_app("Tips", tips.app)
 
-        
-        if app == "Conta":
-            login.app()    
-        if app == "CS":
-            cs.app()        
-        if app == 'Tips':
-            tips.app()
-                 
-             
-    run()    
+    with st.sidebar:
+        app = option_menu(
+            menu_title='FDA ',
+            options=['Conta', 'CS', 'Tips'],
+            icons=['house-fill', 'person-circle', 'trophy-fill', 'chat-fill', 'info-circle-fill'],
+            menu_icon='chat-text-fill',
+            default_index=1,
+            styles={
+                "container": {"padding": "5!important", "background-color": 'black'},
+                "icon": {"color": "white", "font-size": "23px"},
+                "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "blue"},
+                "nav-link-selected": {"background-color": "#02ab21"},
+            }
+        )
+
+    if app == "Conta":
+        login.app()
+    if app == "CS":
+        cs.app()
+    if app == 'Tips':
+        tips.app()
+
+run()
+
 
 
