7e8d2667bd9638c35c079b9f937c5e3a3818113e
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-16 19:08:10 -0300

    Update login.py

--- login.py
+++ login.py
@@ -4,11 +4,14 @@ from firebase_admin import firestore
 from firebase_admin import credentials
 from firebase_admin import auth
 
+# Importe a função do arquivo cs.py
+import cs
 
 cred = credentials.Certificate("football-data-analysis-29975-firebase-adminsdk-ejqyp-bd278ebaff.json")
 firebase_admin.initialize_app(cred)
+
 def app():
-# Usernm = []
+    # Usernm = []
     st.title('Bem Vindo :red[Football Data Analysis]:')
 
     if 'username' not in st.session_state:
@@ -16,71 +19,15 @@ def app():
     if 'useremail' not in st.session_state:
         st.session_state.useremail = ''
 
+    # Resto do seu código de autenticação...
 
-
-    def f(): 
-        try:
-            user = auth.get_user_by_email(email)
-            print(user.uid)
-            st.session_state.username = user.uid
-            st.session_state.useremail = user.email
-            
-            global Usernm
-            Usernm=(user.uid)
-            
-            st.session_state.signedout = True
-            st.session_state.signout = True    
-  
-            
-        except: 
-            st.warning('Login Failed')
-
-    def t():
-        st.session_state.signout = False
-        st.session_state.signedout = False   
-        st.session_state.username = ''
-
-
-        
-    
-        
-    if "signedout"  not in st.session_state:
-        st.session_state["signedout"] = False
-    if 'signout' not in st.session_state:
-        st.session_state['signout'] = False    
-        
-
-        
-    
-    if  not st.session_state["signedout"]: # only show if the state is False, hence the button has never been clicked
-        choice = st.selectbox('Login/Signup',['Login','Sign up'])
-        email = st.text_input('Email Address')
-        password = st.text_input('Password',type='password')
-        
-
-        
-        if choice == 'Sign up':
-            username = st.text_input("Enter  your unique username")
-            
-            if st.button('Create my account'):
-                user = auth.create_user(email = email, password = password,uid=username)
-                
-                st.success('Account created successfully!')
-                st.markdown('Please Login using your email and password')
-                st.balloons()
-        else:
-            # st.button('Login', on_click=f)          
-            st.button('Login', on_click=f)
-            
-            
     if st.session_state.signout:
-                st.text('Name '+st.session_state.username)
-                st.text('Email id: '+st.session_state.useremail)
-                st.button('Sign out', on_click=t) 
-            
-                
-    
+        st.text('Name ' + st.session_state.username)
+        st.text('Email id: ' + st.session_state.useremail)
+        st.button('Sign out', on_click=t)
 
-                            
-    def ap():
-        st.write('cs')
+    # Chame a função ap() para exibir o seu conteúdo
+    ap()
+
+    # Chame a função show_cs_page() do módulo cs
+    cs.show_cs_page()
