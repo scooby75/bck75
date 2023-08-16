7481d1804694e047f28ceb7f690a250d1fc7b2ce
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-16 09:46:03 -0300

    Delete content_page.py

--- content_page.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def content_page(user):
-    st.title("Página de Conteúdo")
-    
-    if user is not None:
-        st.write("Este é o conteúdo da página após o login.")
-    else:
-        st.warning("Você não está autenticado. Faça login para ver o conteúdo.")
