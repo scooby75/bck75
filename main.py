0d689351a78cbaa6443731dcfad39f6eece05930
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-16 15:05:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 
-import trending, test, your, about
+import login, cs, tips
 st.set_page_config(
         page_title="Football Data Analysis",
 )
@@ -23,12 +23,31 @@ class MultiApp:
         })
 
     def run():
-           
-        if app == "Account":
-            test.app()    
-       
-             
-          
+        # app = st.sidebar(
+        with st.sidebar:        
+            app = option_menu(
+                menu_title='FDA ',
+                options=['Conta','CS','Tips'],
+                icons=['house-fill','person-circle','trophy-fill','chat-fill','info-circle-fill'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important","background-color":'black'},
+        "icon": {"color": "white", "font-size": "23px"}, 
+        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
+        "nav-link-selected": {"background-color": "#02ab21"},}
+                
+                )
+
+        
+        if app == "Conta":
+            login.app()    
+        if app == "CS":
+            cs.app()        
+        if app == 'Tips':
+            tips.app()
+                 
              
     run()    
+
 
