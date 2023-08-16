0e6d0d528cb1dfdc8bf0ae4c2069fdf003648ce3
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-16 10:14:59 -0300

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ cred = credentials.Certificate("pondering-5ff7c-c033cfade319.json")
 firebase_admin.initialize_app(cred)
 def app():
 # Usernm = []
-    st.title('Bem vindo ao Football Data Analysis')
+    st.title('Bem Vindo ao : [Football Data Analysis]')
 
     if 'username' not in st.session_state:
         st.session_state.username = ''
