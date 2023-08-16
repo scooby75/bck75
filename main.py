1bef9af2cfa92c5c6249154bea90500a67949f09
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-16 14:54:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-import trending, test, your, about
 
+from streamlit_option_menu import option_menu
+
+
+import home, trending, test, your, about
 st.set_page_config(
-    page_title="Football Data Analysis",
+        page_title="Football Data Analysis",
 )
+
+
 
 class MultiApp:
 
@@ -12,44 +16,40 @@ class MultiApp:
         self.apps = []
 
     def add_app(self, title, func):
+
         self.apps.append({
             "title": title,
             "function": func
         })
 
-    def run(self):
-        app_names = [app["title"] for app in self.apps]
-        active_app = st.selectbox("Selecione uma página:", app_names)
+    def run():
+        # app = st.sidebar(
+        with st.sidebar:        
+            app = option_menu(
+                menu_title='Pondering ',
+                options=['Account','Trending','Your Posts','about'],
+                icons=['house-fill','person-circle','trophy-fill','chat-fill','info-circle-fill'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important","background-color":'black'},
+        "icon": {"color": "white", "font-size": "23px"}, 
+        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
+        "nav-link-selected": {"background-color": "#02ab21"},}
+                
+                )
 
-        for app in self.apps:
-            if active_app == app["title"]:
-                app["function"]()  # Chama a função do aplicativo correspondente
-
-def login():
-    st.session_state.sync()  # Sincroniza o estado da sessão
-    if not st.session_state.is_logged_in:
-        username = st.text_input("Nome de usuário")
-        password = st.text_input("Senha", type="password")
-        login_button = st.button("Entrar")
-
-        if login_button:
-            # Faça a validação do login aqui
-            # Se as credenciais estiverem corretas, defina is_logged_in como True
-            if username == "usuario" and password == "senha":
-                st.session_state.is_logged_in = True
-            else:
-                st.error("Credenciais inválidas")
-
-def main():
-    login()
-
-    if st.session_state.is_logged_in:
-        app.run()
-
-if __name__ == "__main__":
-    app = MultiApp()
-    app.add_app("Conta", test.app)  # Substitua test.app pela função apropriada do seu aplicativo
-    
-    st.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=230", use_column_width=True)
-    main()
+        
+        if app == "Account":
+            test.app()    
+        if app == "Trending":
+            trending.app()        
+        if app == 'Your Posts':
+            your.app()
+        if app == 'about':
+            about.app()    
+             
+          
+             
+    run()    
 
