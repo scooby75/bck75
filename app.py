69ac4aad1205f6465f91105368d66ee711151195
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 00:37:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
 import streamlit as st
+from streamlit_multiselect import MultiApp
 from pages import login, page1, page2, page3
 
-# Função principal
-def main():
-    st.set_page_config(page_title="Controle de Acesso", layout="wide")
+# Crie uma instância do MultiApp
+app = MultiApp()
 
-    if not st.session_state.logged_in:
-        st.session_state.logged_in = login.login()
+# Adicione as páginas ao MultiApp
+app.add_app("Login", login.app)
+app.add_app("Página 1", page1.app)
+app.add_app("Página 2", page2.app)
+app.add_app("Página 3", page3.app)
 
-    if st.session_state.logged_in:
-        st.write("Menu de Navegação")
-        page = st.selectbox("Selecione a página", ["Página 1", "Página 2", "Página 3"])
+# Execute o MultiApp
+app.run()
 
-        if page == "Página 1":
-            page1.show_content()
-        elif page == "Página 2":
-            page2.show_content()
-        elif page == "Página 3":
-            page3.show_content()
-
-if __name__ == "__main__":
-    main()
