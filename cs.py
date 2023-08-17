630790410d2ff1bbd1d4d48797463ba170de7ede
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 11:55:15 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-def tab1():
-    st.title("CS")
-    st.subheader("DAVI")
+def cs_page():
+    st.title("DAVI")
+
+    # Conteúdo específico da página de CS
+    st.write("Bem-vindo à página de análise de Counter-Strike!")
+
+    # Exemplos de conteúdo (substitua com seus próprios dados)
+    st.subheader("Estatísticas do Último Jogo:")
+    st.write("Mapa: Mirage")
+    st.write("Pontuação final: CT 16 - T 12")
+    st.write("Melhor jogador: Player123")
+
+    # Gráficos ou tabelas relevantes podem ser adicionados aqui
+
+    st.subheader("Dicas de Estratégia:")
+    st.write("Dica 1: Comunicação constante é essencial.")
+    st.write("Dica 2: Controle de mapa é crucial para o sucesso.")
 
