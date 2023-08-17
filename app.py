d83225ee729ced8dfe72087a4c6f941a80721456
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 11:54:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ from jogos import jogos_do_dia_page
 from cs import cs_page
 from tips import tips_page
 
-#st.set_page_config(page_title="Probabilidade de Placar", layout="wide")
-
 def main():
-   
     # Verifica se o estado de sessão "logged_in" já existe
     if not hasattr(st.session_state, "logged_in"):
         st.session_state.logged_in = False
@@ -18,19 +15,20 @@ def main():
     else:
         # Barra lateral com imagem e informações
         st.sidebar.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660")
-        st.sidebar.header("Footbal Data Analysis")
+        st.sidebar.header("Football Data Analysis")  # Corrigido "Footbal" para "Football"
 
         # Mostra informações do usuário e botão de logout na barra lateral
         st.sidebar.write(f"Logado como: {st.session_state.username}")
-        st.sidebar.button("Logout", key="logout_button", on_click=logout)
+        if st.sidebar.button("Logout", key="logout_button"):
+            logout()
 
         # Cria abas para diferentes páginas
-        tab0, tab1, tab2 = st.tabs(["Jogos do Dia", "CS", "Tips"])
-        if tab0 == "Jogos do Dia":
+        tab0, tab1, tab2 = st.sidebar.beta_columns(3)
+        if tab0.button("Jogos do Dia"):
             jogos_do_dia_page()
-        elif tab1 == "CS":
+        if tab1.button("CS"):
             cs_page()
-        elif tab2 == "Tips":
+        if tab2.button("Tips"):
             tips_page()
 
 if __name__ == "__main__":
