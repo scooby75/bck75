0d83fa013762730b59e655aae247e767a4a061e3
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 08:42:24 -0300

    Update jogos.py

--- jogos.py
+++ jogos.py
@@ -8,7 +8,7 @@ def jogos_do_dia_page():
     st.subheader("Jogos do Dia")
     st.text("A base de dados é atualizada diariamente e as odds de referência são da Bet365")
 
-    #@st.cache_data(ttl=dt.timedelta(hours=24))
+    @st.cache_data(ttl=dt.timedelta(hours=24))
     def load_base():
         # url = "https://github.com/futpythontrader/YouTube/blob/main/Jogos_do_Dia_FlashScore/2023-08-03_Jogos_do_Dia_FlashScore.csv?raw=true"
         url = "https://github.com/scooby75/bdfootball/blob/main/jogos_do_dia.csv?raw=true"
@@ -29,11 +29,20 @@ def jogos_do_dia_page():
 
     df2 = load_base()
 
+    # Debug: Exibir as primeiras linhas do DataFrame
+    st.write("Primeiras linhas do DataFrame:")
+    st.write(df2.head())
+
+    # Debug: Verificar se as colunas especificadas estão presentes
+    st.write("Colunas do DataFrame:")
+    st.write(df2.columns)
+
     # Select the specific columns to display in the "Jogos Filtrados" table
     columns_to_display = [
         'Date', 'Time', 'League', 'Home', 'Away', 'Round', 'FT_Odd_H', 'FT_Odd_D', 'FT_Odd_A', 'FT_Odd_Over25', 'FT_Odd_Under25', 'FT_Odd_BTTS_Yes' 
     ]
-    st.dataframe(df2[columns_to_display])
+    st.write("Tabela de Dados:")
+    st.write(df2[columns_to_display])
 
     # Créditos
     st.text("Desenvolvido por Lyssandro Silveira")
