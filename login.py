48759a1fc37e422b25dcd37c866b29456241e112
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 00:07:19 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit.components.v1 import components
 
 # Dicionário de usuários (você pode substituir isso por um banco de dados real)
 users = {
@@ -8,7 +7,6 @@ users = {
     'user3': 'password3'
 }
 
-# Função para fazer o login
 def login():
     st.title("Login")
 
@@ -24,7 +22,3 @@ def login():
 
     return False
 
-# Função para exibir o conteúdo da página após o login
-def show_content():
-    st.title("Conteúdo Restrito")
-    st.write("Bem-vindo à página de conteúdo restrito.")
