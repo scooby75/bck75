a11f0a655ff281b4f450b982cee2813819b88c5c
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 06:31:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,10 @@ def login_page():
     password = st.text_input("Senha", type="password")
     if st.button("Entrar"):
         if username in valid_users and valid_users[username] == password:
-            st.success("Login bem-sucedido!")
-            return True
+            st.session_state.logged_in = True
+            st.session_state.login_time = datetime.datetime.now()
         else:
             st.error("Credenciais inválidas.")
-    return False
 
 def jogos_do_dia_page():
     st.title("Jogos do Dia")
@@ -33,28 +32,19 @@ def tips_page():
 
 def main():
     st.set_page_config(page_title="Meu Sistema", layout="wide")
-    logged_in = st.session_state.get("logged_in", False)
-    
-    if not logged_in:
-        if login_page():
-            st.session_state.logged_in = True
-            st.session_state.login_time = datetime.datetime.now()
-    else:
-        current_time = datetime.datetime.now()
-        login_time = st.session_state.login_time
-        time_elapsed = current_time - login_time
+    if not hasattr(st.session_state, "logged_in"):
+        st.session_state.logged_in = False
 
-        if time_elapsed.total_seconds() > 24 * 3600:  # 24 horas
-            st.session_state.logged_in = False
-            st.session_state.login_time = None
-        else:
-            st_tabs = st.tabs(["Jogos do Dia", "CS", "Tips"])
-            if st_tabs == "Jogos do Dia":
-                jogos_do_dia_page()
-            elif st_tabs == "CS":
-                cs_page()
-            elif st_tabs == "Tips":
-                tips_page()
+    if not st.session_state.logged_in:
+        login_page()
+    else:
+        st_tabs = st.tabs(["Jogos do Dia", "CS", "Tips"])
+        if st_tabs == "Jogos do Dia":
+            jogos_do_dia_page()
+        elif st_tabs == "CS":
+            cs_page()
+        elif st_tabs == "Tips":
+            tips_page()
 
 if __name__ == "__main__":
     main()
