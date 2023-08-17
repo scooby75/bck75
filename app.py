40edeb23cf87a2782059cd7b3d0bc21a61498fe1
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 08:05:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import datetime
-from SessionState import SessionState  # Importa a biblioteca SessionState
 
 # Dados de exemplo para simular o login (substitua por um mecanismo seguro em um ambiente de produção)
 valid_users = {
@@ -8,9 +7,9 @@ valid_users = {
     "usuario2": "senha456"
 }
 
-def login_page(session):
-    st.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660", width=280)
-    st.title("Football Data Analysis")
+def login_page():
+    st.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660", width=300)
+    st.title("Página de Login")
     username = st.text_input("Nome de usuário")
     password = st.text_input("Senha", type="password")
 
@@ -18,9 +17,9 @@ def login_page(session):
 
     if login_button:  # Verificar se o botão foi clicado
         if username in valid_users and valid_users[username] == password:
-            session.logged_in = True
-            session.username = username  # Armazena o nome de usuário
-            session.login_time = datetime.datetime.now()
+            st.session_state.logged_in = True
+            st.session_state.username = username  # Armazena o nome de usuário
+            st.session_state.login_time = datetime.datetime.now()
         else:
             st.error("Credenciais inválidas.")
 
@@ -33,35 +32,36 @@ def cs_page():
     # Coloque aqui a lógica e o conteúdo da página de CS
 
 def tips_page():
-    st.title("Tips")
-    # Coloque aqui a lógica e o conteúdo da página de Tips
+    st.title("Dicas")
+    # Coloque aqui a lógica e o conteúdo da página de Dicas
 
 def main():
-    st.set_page_config(page_title="Football Data Analysis", layout="wide")
-    session_state = SessionState.get(logged_in=False)  # Inicializa o estado da sessão
+    st.set_page_config(page_title="Meu Sistema", layout="wide")
 
-    if not session_state.logged_in:
-        login_page(session_state)
+    if not hasattr(st.session_state, "logged_in"):
+        st.session_state.logged_in = False
+
+    if not st.session_state.logged_in:
+        login_page()
     else:
         st.sidebar.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660")
-        st.sidebar.markdown("by Lyssandro Silveira")
+        st.sidebar.markdown("por Lyssandro Silveira")
 
-        st.sidebar.write(f"Logado como: {session_state.username}")  # Mostra o nome do usuário na barra lateral
+        st.sidebar.write(f"Logado como: {st.session_state.username}")  # Mostra o nome do usuário na barra lateral
         st.sidebar.button("Logout", key="logout_button", on_click=logout)  # Botão de logout na barra lateral
 
-        st_tabs = st.tabs(["Jogos do Dia", "CS", "Tips"])
+        st_tabs = st.tabs(["Jogos do Dia", "CS", "Dicas"])
         if st_tabs == "Jogos do Dia":
             jogos_do_dia_page()
         elif st_tabs == "CS":
             cs_page()
-        elif st_tabs == "Tips":
+        elif st_tabs == "Dicas":
             tips_page()
 
 def logout():
-    session_state = SessionState.get()
-    session_state.logged_in = False
-    session_state.username = None
-    session_state.login_time = None
+    st.session_state.logged_in = False
+    st.session_state.pop("username", None)
+    st.session_state.pop("login_time", None)
 
 if __name__ == "__main__":
     main()
