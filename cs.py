dddad564c600c77e334c3727288d336468055c52
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 11:08:45 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -3,7 +3,7 @@ import pandas as pd
 from scipy.stats import poisson
 
 # Set Streamlit page configuration
-st.set_page_config(page_title="Football Data Analysis", layout="wide")
+st.set_page_config(page_title="Probabilidade de Placar", layout="wide")
 
 def cs_page():
     st.title("CS")
