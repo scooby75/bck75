9fe7a78987482b7b46e1d3c951a6aaefa09ad955
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 00:28:22 -0300

    Update page1.py

--- page1.py
+++ page1.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-def show_content(title):
-    st.title(title)  # Defina o título da página
-    st.write("Conteúdo da Página 1.")
+def show_content():
+    st.title("Conteúdo da Página 1")
+    st.write("Bem-vindo à Página 1!")
