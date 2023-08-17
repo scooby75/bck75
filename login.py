68c770aa45876e625e65d3e87014e79601f620f8
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 00:38:27 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,23 +1,14 @@
 import streamlit as st
 
-# Dicionário de usuários (você pode substituir isso por um banco de dados real)
-users = {
-    'user1': 'password1',
-    'user2': 'password2',
-    'user3': 'password3'
-}
-
-def login():
+def app():
     st.title("Login")
 
     username = st.text_input("Usuário")
     password = st.text_input("Senha", type='password')
 
     if st.button("Entrar"):
-        if username in users and users[username] == password:
+        if username == "user" and password == "pass":
             st.success("Login realizado com sucesso!")
-            return True
         else:
             st.error("Usuário ou senha incorretos.")
 
-    return False
