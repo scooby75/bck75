6e3ccaa270ccd1cca888da0c2b07cddd73217999
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 10:20:55 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -94,6 +94,7 @@ def cs_page():
     # Filtrar o DataFrame temporário para incluir apenas os 6 placares mais prováveis
         prob_game_df = prob_game_df[top_placares]
 
+with tab2:
     # Formatar e exibir a tabela
         formatted_df = prob_game_df.applymap(lambda x: f"{x:.1f}%")
         st.dataframe(formatted_df)
