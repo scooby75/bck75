ebbb40a4c02c7e40a1b2fa599821ee3fa5a7c140
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 10:22:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,8 @@
 import streamlit as st
-import datetime
-
-# Dados de exemplo para simular o login (substitua por um mecanismo seguro em um ambiente de produção)
-valid_users = {
-    "lsilveira": "senha123",
-    "usuario2": "senha456"
-}
-
-def login_page():
-    st.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660", width=240)
-    st.title("Football Data Analysis")
-    username = st.text_input("Nome de usuário")
-    password = st.text_input("Senha", type="password")
-
-    login_button = st.button("Entrar")  # Criar botão de login
-
-    if login_button:  # Verificar se o botão foi clicado
-        if username in valid_users and valid_users[username] == password:
-            st.session_state.logged_in = True
-            st.session_state.username = username  # Armazena o nome de usuário
-            st.session_state.login_time = datetime.datetime.now()
-        else:
-            st.error("Credenciais inválidas.")
-
-def jogos_do_dia_page():
-    st.title("Jogos do Dia")
-    # Coloque aqui a lógica e o conteúdo da página de jogos do dia
-
-def cs_page():
-    st.title("CS")
-    # Coloque aqui a lógica e o conteúdo da página de CS
-
-def tips_page():
-    st.title("Tips")
-    # Coloque aqui a lógica e o conteúdo da página de Tips
+from login import login_page, logout
+from jogos import jogos_do_dia_page
+from cs import cs_page
+from tips import tips_page
 
 def main():
     st.set_page_config(page_title="Football Data Analysis", layout="wide")
@@ -46,8 +15,8 @@ def main():
         st.sidebar.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660")
         st.sidebar.markdown("by Lyssandro Silveira")
 
-        st.sidebar.write(f"Logado como: {st.session_state.username}")  # Mostra o nome do usuário na barra lateral
-        st.sidebar.button("Logout", key="logout_button", on_click=logout)  # Botão de logout na barra lateral
+        st.sidebar.write(f"Logado como: {st.session_state.username}")
+        st.sidebar.button("Logout", key="logout_button", on_click=logout)
 
         st_tabs = st.tabs(["Jogos do Dia", "CS", "Tips"])
         if st_tabs == "Jogos do Dia":
@@ -57,10 +26,6 @@ def main():
         elif st_tabs == "Tips":
             tips_page()
 
-def logout():
-    st.session_state.logged_in = False
-    st.session_state.pop("username", None)
-    st.session_state.pop("login_time", None)
-
 if __name__ == "__main__":
     main()
+
