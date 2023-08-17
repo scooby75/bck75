2f6e277f7f3ae74157977a6277d609abdf74b0cb
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 11:05:40 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 from scipy.stats import poisson
 
+# Set Streamlit page configuration
+st.set_page_config(page_title="Football Data Analysis", layout="wide")
+
 def cs_page():
     st.title("CS")
     
