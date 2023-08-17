1e4f63a2b6030e326a02f7393664693885aa5b8b
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 08:06:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ valid_users = {
 }
 
 def login_page():
-    st.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660", width=300)
+    st.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660", width=270)
     st.title("Página de Login")
     username = st.text_input("Nome de usuário")
     password = st.text_input("Senha", type="password")
@@ -32,12 +32,11 @@ def cs_page():
     # Coloque aqui a lógica e o conteúdo da página de CS
 
 def tips_page():
-    st.title("Dicas")
-    # Coloque aqui a lógica e o conteúdo da página de Dicas
+    st.title("Tips")
+    # Coloque aqui a lógica e o conteúdo da página de Tips
 
 def main():
     st.set_page_config(page_title="Meu Sistema", layout="wide")
-
     if not hasattr(st.session_state, "logged_in"):
         st.session_state.logged_in = False
 
@@ -45,17 +44,17 @@ def main():
         login_page()
     else:
         st.sidebar.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660")
-        st.sidebar.markdown("por Lyssandro Silveira")
+        st.sidebar.markdown("by Lyssandro Silveira")
 
         st.sidebar.write(f"Logado como: {st.session_state.username}")  # Mostra o nome do usuário na barra lateral
         st.sidebar.button("Logout", key="logout_button", on_click=logout)  # Botão de logout na barra lateral
 
-        st_tabs = st.tabs(["Jogos do Dia", "CS", "Dicas"])
+        st_tabs = st.tabs(["Jogos do Dia", "CS", "Tips"])
         if st_tabs == "Jogos do Dia":
             jogos_do_dia_page()
         elif st_tabs == "CS":
             cs_page()
-        elif st_tabs == "Dicas":
+        elif st_tabs == "Tips":
             tips_page()
 
 def logout():
