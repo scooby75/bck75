3e7a2d07444c16c5237d62385fb1347bba8ce6ff
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-17 11:42:48 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -1,13 +1,7 @@
 import streamlit as st
 
-# Set Streamlit page configuration
-# st.set_page_config(page_title="Probabilidade de Placar", layout="wide")
-
 def cs_page():
     st.title("CS")
-    
-    # Iniciar aplicativo Streamlit
     st.subheader("DAVI")
 
-# Call the function to run the Streamlit app
 cs_page()
