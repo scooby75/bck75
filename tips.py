d2f07c958fc8cbc68a91d70940d8f48d7ae8ed1c
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-18 06:24:28 -0300

    Update tips.py

--- tips.py
+++ tips.py
@@ -6,45 +6,17 @@ def tips_page():
     # URL do arquivo CSV
     url = "https://github.com/scooby75/bdfootball/blob/main/jogos_do_dia.csv?raw=true"
 
-    # Carregar o arquivo CSV em um dataframe
-    df = pd.read_csv(url)
+        # Filtrando os jogos com valores de "FT_Odd_H" eh menor que 1.50 e "Round_Num" maior ou igual a 10
+    lay0x2_df = df[(df["FT_Odd_H"] <= 1.50) & (df["Round_Num"] >= 10)]
 
-    # Rename the columns
-    df.rename(columns={
-        'FT_Odds_H': 'FT_Odd_H',
-        'FT_Odds_D': 'FT_Odd_D',
-        'FT_Odds_A': 'FT_Odd_A',
-        'FT_Odds_Over25': 'FT_Odd_Over25',
-        'FT_Odds_Under25': 'FT_Odd_Under25',
-        'Odds_BTTS_Yes': 'FT_Odd_BTTS_Yes',
-        'Rodada': 'Round',
-    }, inplace=True)
-
-    # Função para extrair o número do texto "ROUND N"
-    def extrair_numero_round(text):
-        if isinstance(text, int):
-            return text
-        match = re.search(r'\d+', text)
-        if match:
-            return int(match.group())
-        return None
-
-    # Aplicando a função para extrair o número do "Round" e criando uma nova coluna "Round_Num"
-    df["Round_Num"] = df["Round"].apply(extrair_numero_round)
-
-    # Filtrando os jogos com valores de "FT_Odd_H" entre 1.40 e 2.0 e "Round_Num" maior ou igual a 10
-    eventos_raros_df = df[(df["FT_Odd_H"] >= 1.71) & (df["FT_Odd_H"] <= 2.4) & (df["FT_Odd_Over25"] >= 2.01) & (df["Round_Num"] >= 10)]
-
-    # Selecionar apenas as colunas desejadas: Date, Time, League, Home e Away
+# Selecionar apenas as colunas desejadas: Date, Time, League, Home e Away
     colunas_desejadas = ["Date", "Time", "League", "Home", "Away"]
-    eventos_raros_df = eventos_raros_df[colunas_desejadas]
+    lay0x2_df = lay0x2_df[colunas_desejadas]
 
-    # Exibir o dataframe "Eventos Raros"
-    st.subheader("Lay Goleada Casa")
-    st.text("Apostar em Lay Goleada Casa, Odd máxima 30")
-    st.dataframe(eventos_raros_df)
-
-    # ... Repetir o padrão para outras seções ...
+# Exibir o dataframe "Eventos Raros"
+    st.subheader("Lay 0x2")
+    st.text("Apostar em Lay 0x2, Odd máxima 50")
+    st.dataframe(lay0x2_df)
 
 # Chamar a função para iniciar o aplicativo
 tips_page()
