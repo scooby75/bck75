14c270f76585abdf33444623744af6724cde783d
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-18 08:31:46 -0300

    Update jogos.py

--- jogos.py
+++ jogos.py
@@ -9,8 +9,8 @@ def jogos_do_dia_page():
     # Load the data
     @st.cache_data(ttl=dt.timedelta(hours=24))
     def load_base():
-        url = "https://github.com/scooby75/bdfootball/blob/main/Jogos_do_Dia.csv?raw=true"
-        data_jogos = pd.read_csv(url)
+        url = "https://github.com/scooby75/bdfootball/blob/main/jogos_do_dia.xlsx?raw=true"
+        data_jogos = pd.read_excel(url)
 
         # Rename the columns
         data_jogos.rename(columns={
