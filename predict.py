1ff88388e7adfe844d5badfa3884eac5658d4ee4
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-18 19:20:03 -0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -29,14 +29,29 @@ def predict_page():
     df2 = load_base()
 
     # Create sliders for filter conditions
-    prob_vitoria_home_threshold = st.slider("Prob de Vitória Home", 0, 100, 0)
-    prob_vitoria_away_threshold = st.slider("Prob de Vitória Away", 0, 100, 0)
-    prob_over25_home_threshold = st.slider("Prob Over 2.5 Home", 0, 100, 0)
-    prob_over25_away_threshold = st.slider("Prob Over 2.5 Away", 0, 100, 0)
-    media_gols_h_threshold = st.slider("Media Gols H", 0.0, 10.0, 0.0)
-    media_gols_a_threshold = st.slider("Media Gols A", 0.0, 10.0, 0.0)
-    ppg_h_threshold = st.slider("PPG_H", 0.0, 3.0, 0.0)
-    ppg_a_threshold = st.slider("PPG_A", 0.0, 3.0, 0.0)
+    prob_vitoria_home_min = st.slider("Prob de Vitória Home (mínimo)", 0, 100, 0)
+    prob_vitoria_home_max = st.slider("Prob de Vitória Home (máximo)", prob_vitoria_home_min, 100, 100)
+    
+    prob_vitoria_away_min = st.slider("Prob de Vitória Away (mínimo)", 0, 100, 0)
+    prob_vitoria_away_max = st.slider("Prob de Vitória Away (máximo)", prob_vitoria_away_min, 100, 100)
+
+    prob_over25_home_min = st.slider("Prob Over 2.5 Home (mínimo)", 0, 100, 0)
+    prob_over25_home_max = st.slider("Prob Over 2.5 Home (máximo)", prob_over25_home_min, 100, 100)
+    
+    prob_over25_away_min = st.slider("Prob Over 2.5 Away (mínimo)", 0, 100, 0)
+    prob_over25_away_max = st.slider("Prob Over 2.5 Away (máximo)", prob_over25_away_min, 100, 100)
+
+    media_gols_h_min = st.slider("Media Gols H (mínimo)", 0.0, 10.0, 0.0)
+    media_gols_h_max = st.slider("Media Gols H (máximo)", media_gols_h_min, 10.0, 10.0)
+    
+    media_gols_a_min = st.slider("Media Gols A (mínimo)", 0.0, 10.0, 0.0)
+    media_gols_a_max = st.slider("Media Gols A (máximo)", media_gols_a_min, 10.0, 10.0)
+
+    ppg_h_min = st.slider("PPG_H (mínimo)", 0.0, 3.0, 0.0)
+    ppg_h_max = st.slider("PPG_H (máximo)", ppg_h_min, 3.0, 3.0)
+    
+    ppg_a_min = st.slider("PPG_A (mínimo)", 0.0, 3.0, 0.0)
+    ppg_a_max = st.slider("PPG_A (máximo)", ppg_a_min, 3.0, 3.0)
 
     # Specify columns to display in the sorted data
     columns_to_display = [
@@ -47,36 +62,34 @@ def predict_page():
     if not df2.empty:
         filtered_data = df2.copy()
 
-        # Aplicar filtro Prob de Vitória Home
-        filtered_data = filtered_data[filtered_data['Prob_Vitoria_Home'] >= prob_vitoria_home_threshold]
+        # Apply filters with ranges
+        filtered_data = filtered_data[
+            (filtered_data['Prob_Vitoria_Home'] >= prob_vitoria_home_min) &
+            (filtered_data['Prob_Vitoria_Home'] <= prob_vitoria_home_max) &
+            (filtered_data['Prob_Vitoria_Away'] >= prob_vitoria_away_min) &
+            (filtered_data['Prob_Vitoria_Away'] <= prob_vitoria_away_max) &
+            (filtered_data['Prob_Over25_Home'] >= prob_over25_home_min) &
+            (filtered_data['Prob_Over25_Home'] <= prob_over25_home_max) &
+            (filtered_data['Prob_Over25_Away'] >= prob_over25_away_min) &
+            (filtered_data['Prob_Over25_Away'] <= prob_over25_away_max) &
+            (filtered_data['Media_Gols_H'] >= media_gols_h_min) &
+            (filtered_data['Media_Gols_H'] <= media_gols_h_max) &
+            (filtered_data['Media_Gols_A'] >= media_gols_a_min) &
+            (filtered_data['Media_Gols_A'] <= media_gols_a_max) &
+            (filtered_data['PPG_H'] >= ppg_h_min) &
+            (filtered_data['PPG_H'] <= ppg_h_max) &
+            (filtered_data['PPG_A'] >= ppg_a_min) &
+            (filtered_data['PPG_A'] <= ppg_a_max)
+        ]
 
-        # Aplicar filtro Prob de Vitória Away
-        filtered_data = filtered_data[filtered_data['Prob_Vitoria_Away'] >= prob_vitoria_away_threshold]
-
-        # Aplicar filtro Prob Over 2.5 Home
-        filtered_data = filtered_data[filtered_data['Prob_Over25_Home'] >= prob_over25_home_threshold]
-
-        # Aplicar filtro Prob Over 2.5 Away
-        filtered_data = filtered_data[filtered_data['Prob_Over25_Away'] >= prob_over25_away_threshold]
-
-        # Aplicar filtro Media Gols H
-        filtered_data = filtered_data[filtered_data['Media_Gols_H'] >= media_gols_h_threshold]
-
-        # Aplicar filtro Media Gols A
-        filtered_data = filtered_data[filtered_data['Media_Gols_A'] >= media_gols_a_threshold]
-
-        # Aplicar filtro PPG_H
-        filtered_data = filtered_data[filtered_data['PPG_H'] >= ppg_h_threshold]
-
-        # Aplicar filtro PPG_A
-        filtered_data = filtered_data[filtered_data['PPG_A'] >= ppg_a_threshold]
-
-        # Ordenar os dados filtrados por Hora
+        # Sort the filtered data by 'Hora'
         filtered_data = filtered_data.sort_values(by='Hora')
 
+        # Display the filtered data
         st.dataframe(filtered_data[columns_to_display])
     else:
         st.warning("Não existem jogos com esses critérios!")
 
-# Chamar a função para exibir a aplicação web
+# Call the function to display the web application
 predict_page()
+
