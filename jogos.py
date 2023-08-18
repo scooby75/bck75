78b33fac5cdbc8dc5d966f335e736310f90f935b
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-18 07:27:14 -0300

    Update jogos.py

--- jogos.py
+++ jogos.py
@@ -6,6 +6,7 @@ def jogos_do_dia_page():
     st.subheader("Jogos do Dia")
     st.text("A base de dados é atualizada diariamente e as odds de referência são da Bet365")
 
+    # Load the data
     @st.cache_data(ttl=dt.timedelta(hours=24))
     def load_base():
         url = "https://github.com/scooby75/bdfootball/blob/main/jogos_do_dia.csv?raw=true"
@@ -33,32 +34,32 @@ def jogos_do_dia_page():
         'FT_Odd_Over25', 'FT_Odd_Under25', 'FT_Odd_BTTS_Yes'
     ]
 
+    # Filters for selected columns
+    filter_values = {}
+    filter_columns = [
+        ('FT Odds Home', 'selected_ft_odd_h'),
+        ('FT Odds Draw', 'selected_ft_odd_d'),
+        ('FT Odds Away', 'selected_ft_odd_a'),
+        ('FT Odds Over 2.5', 'selected_ft_odd_over25'),
+        ('FT Odds Under 2.5', 'selected_ft_odd_under25'),
+        ('FT Odds BTTS Yes', 'selected_ft_odd_btts_yes')
+    ]
+
     col1, col2 = st.columns(2)
 
-    # Filters for selected columns
-    with col1:
-        selected_ft_odd_h_min = st.number_input("Min FT Odds Home:", min_value=1.0)
-        selected_ft_odd_h_max = st.number_input("Max FT Odds Home:", max_value=10.0)
-        selected_ft_odd_d_min = st.number_input("Min FT Odds Draw:", min_value=1.0)
-        selected_ft_odd_d_max = st.number_input("Max FT Odds Draw:", max_value=10.0)
-        selected_ft_odd_a_min = st.number_input("Min FT Odds Away:", min_value=1.0)
-        selected_ft_odd_a_max = st.number_input("Max FT Odds Away:", max_value=10.0)
+    for i, (label, var_name) in enumerate(filter_columns):
+        with col1 if i < len(filter_columns) // 2 else col2:
+            min_val, max_val = st.slider(f"Filter by {label}:", 0.0, 10.0, (0.0, 10.0))
+            filter_values[var_name] = (min_val, max_val)
 
-    with col2:
-        selected_ft_odd_over25_min = st.number_input("Min FT Odds Over 2.5:", min_value=1.0)
-        selected_ft_odd_over25_max = st.number_input("Max FT Odds Over 2.5:", max_value=10.0)
-        selected_ft_odd_under25_min = st.number_input("Min FT Odds Under 2.5:", min_value=1.0)
-        selected_ft_odd_under25_max = st.number_input("Max FT Odds Under 2.5:", max_value=10.0)
-        selected_ft_odd_btts_yes_min = st.number_input("Min FT Odds BTTS Yes:", min_value=1.0)
-        selected_ft_odd_btts_yes_max = st.number_input("Max FT Odds BTTS Yes:", max_value=10.0)
-
+    # Apply filters to the DataFrame
     filtered_data = df2[
-        (df2['FT_Odd_H'] >= selected_ft_odd_h_min) & (df2['FT_Odd_H'] <= selected_ft_odd_h_max) &
-        (df2['FT_Odd_D'] >= selected_ft_odd_d_min) & (df2['FT_Odd_D'] <= selected_ft_odd_d_max) &
-        (df2['FT_Odd_A'] >= selected_ft_odd_a_min) & (df2['FT_Odd_A'] <= selected_ft_odd_a_max) &
-        (df2['FT_Odd_Over25'] >= selected_ft_odd_over25_min) & (df2['FT_Odd_Over25'] <= selected_ft_odd_over25_max) &
-        (df2['FT_Odd_Under25'] >= selected_ft_odd_under25_min) & (df2['FT_Odd_Under25'] <= selected_ft_odd_under25_max) &
-        (df2['FT_Odd_BTTS_Yes'] >= selected_ft_odd_btts_yes_min) & (df2['FT_Odd_BTTS_Yes'] <= selected_ft_odd_btts_yes_max)
+        (df2['FT_Odd_H'].between(*filter_values['selected_ft_odd_h'])) &
+        (df2['FT_Odd_D'].between(*filter_values['selected_ft_odd_d'])) &
+        (df2['FT_Odd_A'].between(*filter_values['selected_ft_odd_a'])) &
+        (df2['FT_Odd_Over25'].between(*filter_values['selected_ft_odd_over25'])) &
+        (df2['FT_Odd_Under25'].between(*filter_values['selected_ft_odd_under25'])) &
+        (df2['FT_Odd_BTTS_Yes'].between(*filter_values['selected_ft_odd_btts_yes']))
     ]
 
     if not filtered_data.empty:
@@ -68,3 +69,4 @@ def jogos_do_dia_page():
 
 # Call the function to display the web application
 jogos_do_dia_page()
+
