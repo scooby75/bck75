c1d9c6893b50b94fd484240f1ef211f2f7c254e6
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-19 10:49:54 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,10 +1,24 @@
 import streamlit as st
 import datetime
+import sqlite3
+import bcrypt
 
-valid_users = {
-    "lsilveira": "senha123",
-    "usuario2": "senha456"
-}
+def create_db():
+    # Criar e conectar ao banco de dados
+    conn = sqlite3.connect("user_credentials.db")
+    cursor = conn.cursor()
+
+    # Criar a tabela se ela ainda não existir
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT PRIMARY KEY,
+            password TEXT
+        )
+    ''')
+
+    # Salvar as mudanças e fechar a conexão
+    conn.commit()
+    conn.close()
 
 def login_page():
     st.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660", width=240)
@@ -15,14 +29,51 @@ def login_page():
     login_button = st.button("Entrar")
 
     if login_button:
-        if username in valid_users and valid_users[username] == password:
+        conn = sqlite3.connect("user_credentials.db")
+        cursor = conn.cursor()
+
+        cursor.execute('SELECT password FROM users WHERE username = ?', (username,))
+        result = cursor.fetchone()
+
+        if result and bcrypt.checkpw(password.encode('utf-8'), result[0]):
             st.session_state.logged_in = True
             st.session_state.username = username
             st.session_state.login_time = datetime.datetime.now()
         else:
             st.error("Credenciais inválidas.")
 
+        conn.close()
+
+    new_user = st.checkbox("Criar novo usuário")
+
+    if new_user:
+        new_username = st.text_input("Novo usuário")
+        new_password = st.text_input("Nova senha", type="password")
+        confirm_password = st.text_input("Confirmar senha", type="password")
+
+        if new_username and new_password and new_password == confirm_password:
+            conn = sqlite3.connect("user_credentials.db")
+            cursor = conn.cursor()
+
+            cursor.execute('SELECT username FROM users WHERE username = ?', (new_username,))
+            existing_user = cursor.fetchone()
+
+            if existing_user:
+                st.error("Este usuário já existe.")
+            else:
+                hashed_password = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
+
+                cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (new_username, hashed_password))
+                conn.commit()
+                st.success("Novo usuário criado com sucesso.")
+
+            conn.close()
+
 def logout():
     st.session_state.logged_in = False
     st.session_state.pop("username", None)
     st.session_state.pop("login_time", None)
+
+if __name__ == "__main__":
+    create_db()
+    login_page()
