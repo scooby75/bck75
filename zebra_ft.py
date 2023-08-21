b4ef584353b777536fd58283101007731cde54f3
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-21 20:10:21 -0300

    Update zebra_ft.py

--- zebra_ft.py
+++ zebra_ft.py
@@ -20,11 +20,11 @@ def zebra_ft_page():
         'FT_Odds_Over25': 'FT_Odd_Over25',
         'FT_Odds_Under25': 'FT_Odd_Under25',
         'Odds_BTTS_Yes': 'FT_Odd_BTTS_Yes',
-        'Rodada': 'Round',
+        'ROUND': 'Rodada',
     }, inplace=True)
 
     # Função para extrair o número do texto "ROUND N"
-    def extrair_numero_round(text):
+    def extrair_numero_rodada(text):
         if isinstance(text, int):
             return text
         match = re.search(r'\d+', text)
@@ -32,14 +32,14 @@ def zebra_ft_page():
             return int(match.group())
         return None
 
-    # Aplicando a função para extrair o número do "Round" e criando uma nova coluna "Round_Num"
-    df["Round_Num"] = df["Round"].apply(extrair_numero_round)
+    # Aplicando a função para extrair o número do "Rodada" e criando uma nova coluna "Rodada_Num"
+    df["Rodada_Num"] = df["Rodada"].apply(extrair_numero_rodada)
 
 # Filtrando os jogos com valores de "FT_Odd_H" eh menor que 1.50 e "Round_Num" maior ou igual a 10
     layzebraft_df = df[
     (df["FT_Odd_H"] >= 1.4) & (df["FT_Odd_H"] <= 2.1) &
     (df["FT_Odd_A"] >= 5) & (df["FT_Odd_A"] <= 10) &
-    (df["Round_Num"] >= 10)
+    (df["Rodada_Num"] >= 10)
     ]
 
 # Selecionar apenas as colunas desejadas: Date, Time, League, Home e Away
