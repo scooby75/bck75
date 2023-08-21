cdcf8c5e20eab4a013ac4dee216a99a91dbcf25c
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-21 20:51:08 -0300

    Update lay_zebra.py

--- lay_zebra.py
+++ lay_zebra.py
@@ -3,16 +3,16 @@ import pandas as pd
 import re
 
 def lay_zebra_page():
+    # Load Zebra HT Data
+    st.subheader("Lay Zebra HT")
+    st.text("Apostar em Lay visitante, Odd máxima 6")
 
-
- ### Lay Zebra HT ####
-
-# URL do arquivo CSV
+    # URL for the CSV file
     url = "https://github.com/scooby75/bdfootball/blob/main/2023-08-22_Jogos_do_Dia_FS.csv?raw=true"
-  
-    # Carregar o arquivo CSV em um dataframe
+    
+    # Load CSV data into a DataFrame
     df = pd.read_csv(url)
-
+    
     # Rename the columns
     df.rename(columns={
         'FT_Odds_H': 'FT_Odd_H',
@@ -21,10 +21,10 @@ def lay_zebra_page():
         'FT_Odds_Over25': 'FT_Odd_Over25',
         'FT_Odds_Under25': 'FT_Odd_Under25',
         'Odds_BTTS_Yes': 'FT_Odd_BTTS_Yes',
-        'Rodada': 'Round',
+        'ROUND': 'Rodada',
     }, inplace=True)
-
-    # Função para extrair o número do texto "RODADA N"
+    
+    # Function to extract round numbers from text
     def extrair_numero_rodada(text):
         if isinstance(text, int):
             return text
@@ -32,27 +32,26 @@ def lay_zebra_page():
         if match:
             return int(match.group())
         return None
-
-    # Aplicando a função para extrair o número do "Rodada" e criando uma nova coluna "Rodada_Num"
+    
+    # Apply the function to extract round numbers and create a new column "Rodada_Num"
     df["Rodada_Num"] = df["Rodada"].apply(extrair_numero_rodada)
-
-# Filtrando os jogos com valores de "FT_Odd_H" eh menor que 1.50 e "Round_Num" maior ou igual a 10
+    
+    # Filter matches with conditions
     layzebraht_df = df[
-    (df["FT_Odd_H"] >= 1.01) & (df["FT_Odd_H"] <= 1.7) &
-    (df["FT_Odd_A"] >= 5.5) & (df["FT_Odd_A"] <= 10) &
-    (df["Rodada_Num"] >= 10)
+        (df["FT_Odd_H"] >= 1.01) & (df["FT_Odd_H"] <= 1.7) &
+        (df["FT_Odd_A"] >= 5.5) & (df["FT_Odd_A"] <= 10) &
+        (df["Rodada_Num"] >= 10)
     ]
-
-# Selecionar apenas as colunas desejadas: Date, Time, League, Home e Away
+    
+    # Select desired columns: Date, Time, League, Home, and Away
     colunas_desejadas = ["Date", "Time", "League", "Home", "Away"]
     layzebraht_df = layzebraht_df[colunas_desejadas]
-
-# Exibir o dataframe "Eventos Raros"
-    st.subheader("Lay Zebra HT")
-    st.text("Apostar em Lay visitante, Odd máxima 6")
+    
+    # Display the "Lay Zebra HT" DataFrame
     st.dataframe(layzebraht_df)
 
-# Chamar a função para iniciar o aplicativo
+# Call the function to start the Streamlit app
 lay_zebra_page()
+
 
     
