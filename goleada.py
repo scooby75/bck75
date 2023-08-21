9c80d8f3df2c9c5ed5106e33903b845c47c0723e
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-21 20:47:08 -0300

    Update goleada.py

--- goleada.py
+++ goleada.py
@@ -17,11 +17,11 @@ def goleada_page():
         'FT_Odds_Over25': 'FT_Odd_Over25',
         'FT_Odds_Under25': 'FT_Odd_Under25',
         'Odds_BTTS_Yes': 'FT_Odd_BTTS_Yes',        
-        'Rodada': 'Round',
+        'ROUND': 'Rodada',
     }, inplace=True)
 
     # Função para extrair o número do texto "ROUND N"
-    def extrair_numero_round(text):
+    def extrair_numero_rodada(text):
         if isinstance(text, int):
             return text
         match = re.search(r'\d+', text)
@@ -29,11 +29,11 @@ def goleada_page():
             return int(match.group())
         return None
 
-    # Aplicando a função para extrair o número do "Round" e criando uma nova coluna "Round_Num"
-    df["Round_Num"] = df["Round"].apply(extrair_numero_round)
+    # Aplicando a função para extrair o número do "Rodada" e criando uma nova coluna "Rodada_Num"
+    df["Rodada_Num"] = df["Rodada"].apply(extrair_numero_rodada)
 
-    # Filtrando os jogos com valores de "FT_Odd_H" entre 1.40 e 2.0 e "Round_Num" maior ou igual a 10
-    eventos_raros_df = df[(df["FT_Odd_H"] >= 1.71) & (df["FT_Odd_H"] <= 2.4) & (df["FT_Odd_Over25"] >= 2.01) & (df["Round_Num"] >= 10)]
+    # Filtrando os jogos com valores de "FT_Odd_H" entre 1.40 e 2.0 e "Rodada_Num" maior ou igual a 10
+    eventos_raros_df = df[(df["FT_Odd_H"] >= 1.71) & (df["FT_Odd_H"] <= 2.4) & (df["FT_Odd_Over25"] >= 2.01) & (df["Rodada_Num"] >= 10)]
 
     # Selecionar apenas as colunas desejadas: Date, Time, League, Home e Away
     colunas_desejadas = ["Date", "Time", "League", "Home", "Away"]
@@ -46,18 +46,17 @@ def goleada_page():
 
     ### Lay goleada Visitante @@@
     
-# Filtrando os jogos com valores de "FT_Odd_A" entre 1.40 e 2.0 e "Round_Num" maior ou igual a 10
-    eventos_raros2_df = df[(df["FT_Odd_A"] >= 1.71) & (df["FT_Odd_A"] <= 2.4) & (df["FT_Odd_Over25"] >= 2.01) & (df["Round_Num"] >= 10)]
+    # Filtrando os jogos com valores de "FT_Odd_A" entre 1.40 e 2.0 e "Rodada_Num" maior ou igual a 10
+    eventos_raros2_df = df[(df["FT_Odd_A"] >= 1.71) & (df["FT_Odd_A"] <= 2.4) & (df["FT_Odd_Over25"] >= 2.01) & (df["Rodada_Num"] >= 10)]
 
-# Selecionar apenas as colunas desejadas: Date, Time, League, Home e Away
+    # Selecionar apenas as colunas desejadas: Date, Time, League, Home e Away
     colunas_desejadas = ["Date", "Time", "League", "Home", "Away"]
     eventos_raros2_df = eventos_raros2_df[colunas_desejadas]
 
-# Exibir o dataframe "Eventos Raros"
+    # Exibir o dataframe "Eventos Raros"
     st.subheader("Lay Goleada Visitante")
     st.text("Apostar em Lay Goleada Visitante, Odd máxima 30")
     st.dataframe(eventos_raros2_df)
-
 
 # Chamar a função para iniciar o aplicativo
 goleada_page()
