2776a3d9e551510384ae69b3b13cb06c8e9b2512
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-22 06:50:30 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -1,106 +1,61 @@
-import streamlit as st
 import pandas as pd
-from scipy.stats import poisson
+import numpy as np
+import streamlit as st
 
 def cs_page():
-    # URL do arquivo CSV com os dados dos jogos
-    url = "https://github.com/scooby75/bdfootball/blob/main/jogos_do_dia.csv?raw=true"
+    # Load the CSV data from the URL into a DataFrame
+    url = "https://github.com/scooby75/bdfootball/blob/main/bd%202019_2023%20com%20placar.csv?raw=true"
+    bdgeral = pd.read_csv(url)
 
-    # Carregar os dados do arquivo CSV em um DataFrame
-    df = pd.read_csv(url)
+    # Calculate the average goals scored and conceded at home (Home)
+    df_media_gols_home = bdgeral.groupby('Home').agg({'FT_Goals_H': 'mean', 'FT_Goals_A': 'mean'}).reset_index()
+    df_media_gols_home.rename(columns={'FT_Goals_H': 'Mean_Goals_Home', 'FT_Goals_A': 'Mean_Goals_Away'}, inplace=True)
 
-    # Filtrar jogos com round maior ou igual a 10
-    df = df[df['Rodada'] >= 10]
+    # Calculate the average goals scored and conceded away from home (Away)
+    df_media_gols_away = bdgeral.groupby('Away').agg({'FT_Goals_A': 'mean', 'FT_Goals_H': 'mean'}).reset_index()
+    df_media_gols_away.rename(columns={'FT_Goals_A': 'Mean_Goals_Away', 'FT_Goals_H': 'Mean_Goals_Home'}, inplace=True)
 
-    # Filtrar jogos com home menor ou igual a 1.90
-    df = df[(df['FT_Odds_H'] >= 1.40) & (df['FT_Odds_H'] <= 2.4)]
+    # Load the CSV data from another URL into a DataFrame
+    url = "https://github.com/scooby75/bdfootball/blob/main/2023-08-22_Jogos_do_Dia_FS.csv?raw=true"
+    jogosdodia = pd.read_csv(url)
 
-    # Filtrar jogos com home menor ou igual a 1.90
-    df = df[(df['FT_Odds_Under25'] <= 2.20)]
+    # Merge the filtered games with the calculated mean goals data
+    jogos_filtrados = jogosdodia.merge(df_media_gols_home, left_on='Home', right_on='Home')
+    jogos_filtrados = jogos_filtrados.merge(df_media_gols_away, left_on='Away', right_on='Away')
 
-    # Placares para os quais você deseja calcular a probabilidade
-    placares = ['0x0', '1x0', '0x1', '1x1', '2x0', '0x2', '2x1', '1x2', '2x2', '3x0', '0x3', '3x2', '3x3', '4x0', '4x1', '4x2', '4x3', '4x4', '5x0', '5x1', '5x2', '5x3']
+    # Function to calculate the probability of a certain number of goals using the Poisson distribution
+    def poisson_prob(mean, k):
+        return (np.exp(-mean) * mean ** k) / np.math.factorial(k)
 
-    # Lista para armazenar as linhas dos resultados
-    linhas_resultados = []
+    # List to store the results for display
+    results = []
 
-    # Iterar sobre os jogos e calcular as probabilidades para cada placar
-    for index, row in df.iterrows():
-        # Calcular as médias de gols esperados para cada time e o total esperado
-        lambda_home = row['XG_Home_Pre']
-        lambda_away = row['XG_Away_Pre']
-        lambda_total = row['XG_Total_Pre']
+    # Predict the 6 most probable scores for each game
+    for index, row in jogos_filtrados.iterrows():
+        home_team = row['Home']
+        away_team = row['Away']
+        home_mean_goals = row['Mean_Goals_Home_x']
+        away_mean_goals = row['Mean_Goals_Away_x']
 
-        # Calcular as probabilidades usando a distribuição de Poisson
-        probabilidades = []
-        total_prob = 0  # Total de probabilidade para normalização
+        predicted_scores = []
+        for home_goals in range(7):
+            for away_goals in range(7):
+                home_prob = poisson_prob(home_mean_goals, home_goals)
+                away_prob = poisson_prob(away_mean_goals, away_goals)
+                total_prob = home_prob * away_prob
+                predicted_scores.append((home_goals, away_goals, total_prob))
 
-        for placar in placares:
-            placar_split = placar.split('x')
-            gols_home = int(placar_split[0])
-            gols_away = int(placar_split[1])
+        # Sort the predicted scores by probability in descending order
+        predicted_scores.sort(key=lambda x: x[2], reverse=True)
 
-            prob_home = poisson.pmf(gols_home, lambda_home)
-            prob_away = poisson.pmf(gols_away, lambda_away)
-            prob_total = poisson.pmf(gols_home + gols_away, lambda_total)
+        # Add the results to the list
+        for i in range(6):
+            results.append({'Jogo': f"{home_team} vs {away_team}",
+                            'Placar': f"{predicted_scores[i][0]} - {predicted_scores[i][1]}",
+                            'Probabilidade': predicted_scores[i][2]})
 
-            # Aplicar o ajuste de zero inflado para placares "estranhos"
-            if (lambda_home < gols_home) or (lambda_away < gols_away):
-                prob_placar = prob_home * prob_away * prob_total * 1.50
-            else:
-                prob_placar = prob_home * prob_away * prob_total
+    # Display the results using Streamlit
+    st.write(pd.DataFrame(results))
 
-            total_prob += prob_placar
-            probabilidades.append(prob_placar)
-
-        # Normalizar as probabilidades para que a soma seja 100%
-        probabilidades = [prob / total_prob for prob in probabilidades]
-
-        # Criar uma linha para o resultado deste jogo
-        linha_resultado = {
-            'Date': row['Date'],
-            'Time': row['Time'],
-            'League': row['League'],
-            'Home': row['Home'],
-            'Away': row['Away'],
-            'FT_Odds_H': row['FT_Odds_H'],
-            'FT_Odds_D': row['FT_Odds_D'],
-            'FT_Odds_A': row['FT_Odds_A']
-        }
-
-        for i, placar in enumerate(placares):
-            linha_resultado[placar] = round(probabilidades[i] * 100, 2)
-
-        linhas_resultados.append(linha_resultado)
-
-    # Criar um novo DataFrame com os resultados
-    resultado_df = pd.DataFrame(linhas_resultados)
-
-    # Iniciar aplicativo Streamlit
-    st.subheader("Probabilidade de Placar")
-
-    # Loop para exibir os detalhes e a tabela
-    for index, row in resultado_df.iterrows():
-        details1 = f"**Hora:** {row['Time']}  |  **Home:** {row['Home']}  |  **Away:** {row['Away']}"
-        details2 = f"**Odd Casa:** {row['FT_Odds_H']} |  **Odd Empate:** {row['FT_Odds_D']} |  **Odd Visitante:** {row['FT_Odds_A']}"
-        st.write(details1)
-        st.write(details2)
-
-        # Criar um DataFrame temporário apenas com as probabilidades para o jogo atual
-        prob_game_df = resultado_df[placares].iloc[[index]]
-
-        # Selecionar os 6 placares mais prováveis
-        top_placares = prob_game_df.T.nlargest(8, index)[index].index
-
-        # Filtrar o DataFrame temporário para incluir apenas os 6 placares mais prováveis
-        prob_game_df = prob_game_df[top_placares]
-
-        # Formatar e exibir a tabela
-        formatted_df = prob_game_df.applymap(lambda x: f"{x:.1f}%")
-        st.dataframe(formatted_df)
-
-# Chamar a função para executar o aplicativo
+# Call the function to execute the app
 cs_page()
-
-
-
