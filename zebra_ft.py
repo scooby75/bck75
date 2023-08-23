d530b94ffe234b2b5f8be4dff5a862b22c2aac35
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-23 06:09:09 -0300

    Update zebra_ft.py

--- zebra_ft.py
+++ zebra_ft.py
@@ -12,24 +12,29 @@ def zebra_ft_page():
     def load_base():
         url = "https://github.com/scooby75/bdfootball/blob/main/Jogos_do_Dia_FS.csv?raw=true"
         
-        data_jogos = pd.read_csv(url)
-    
-    # Convert the 'Hora' column to a datetime object
-        data_jogos['Time'] = pd.to_datetime(data_jogos['Time'])
-    
-    # Convert the game times to the local time zone (subtracting 3 hours)
-        data_jogos['Time'] = data_jogos['Time'] - pd.to_timedelta('3 hours')
+        df = pd.read_csv(url)
+        
+        # Convert the 'Hora' column to a datetime object
+        df['Time'] = pd.to_datetime(df['Time'])
+        
+        # Convert the game times to the local time zone (subtracting 3 hours)
+        df['Time'] = df['Time'] - pd.to_timedelta('3 hours')
 
-    # Rename the columns
-    df.rename(columns={
-        'FT_Odds_H': 'FT_Odd_H',
-        'FT_Odds_D': 'FT_Odd_D',
-        'FT_Odds_A': 'FT_Odd_A',
-        'FT_Odds_Over25': 'FT_Odd_Over25',
-        'FT_Odds_Under25': 'FT_Odd_Under25',
-        'Odds_BTTS_Yes': 'FT_Odd_BTTS_Yes',
-        'ROUND': 'Rodada',
-    }, inplace=True)
+        # Rename the columns
+        df.rename(columns={
+            'FT_Odds_H': 'FT_Odd_H',
+            'FT_Odds_D': 'FT_Odd_D',
+            'FT_Odds_A': 'FT_Odd_A',
+            'FT_Odds_Over25': 'FT_Odd_Over25',
+            'FT_Odds_Under25': 'FT_Odd_Under25',
+            'Odds_BTTS_Yes': 'FT_Odd_BTTS_Yes',        
+            'ROUND': 'Rodada',
+        }, inplace=True)
+
+        # Apply the function to extract the round number and create a new column "Rodada_Num"
+        df["Rodada_Num"] = df["Rodada"].apply(extrair_numero_rodada)
+
+        return df
 
     # Função para extrair o número do texto "ROUND N"
     def extrair_numero_rodada(text):
@@ -40,8 +45,8 @@ def zebra_ft_page():
             return int(match.group())
         return None
 
-    # Aplicando a função para extrair o número do "Rodada" e criando uma nova coluna "Rodada_Num"
-    df["Rodada_Num"] = df["Rodada"].apply(extrair_numero_rodada)
+    # Call the load_base function to load the data
+    df = load_base()
 
 # Filtrando os jogos com valores de "FT_Odd_H" eh menor que 1.50 e "Round_Num" maior ou igual a 10
     layzebraft_df = df[
