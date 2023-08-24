aca754b425906d838f60547de0f7d2474158c1e3
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-24 17:17:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from ha_025 import ha_025_page
 from lay_zebra import lay_zebra_page
 from zebra_ft import zebra_ft_page
 from scalping import scalping_page
+from goleada import goleada_page
 from session_state import SessionState
 
 def main():
