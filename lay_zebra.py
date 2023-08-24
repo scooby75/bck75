7349ac68f676f1a1b44f17d1e7cd0c988dff92f8
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-24 15:35:52 -0300

    Update lay_zebra.py

--- lay_zebra.py
+++ lay_zebra.py
@@ -12,34 +12,26 @@ def lay_zebra_page():
         st.error("Você não tem permissão para acessar esta página.")
         return
 
-    # Load the data
-    @st.cache_data(ttl=86400.0)  # 24 hours in seconds
-    def load_base():
-        url = "https://github.com/scooby75/bdfootball/blob/main/Jogos_do_Dia_FS.csv?raw=true"
-        
-        df = pd.read_csv(url)
-        
-        # Convert the 'Hora' column to a datetime object
-        df['Time'] = pd.to_datetime(df['Time'])
-        
-        # Convert the game times to the local time zone (subtracting 3 hours)
-        df['Time'] = df['Time'] - pd.to_timedelta('3 hours')
+    # Carrega o dado
+    url = "https://github.com/scooby75/bdfootball/blob/main/Jogos_do_Dia_FS.csv?raw=true"
+    df = pd.read_csv(url)
+    
+    # Convert the 'Hora' column to a datetime object
+    df['Time'] = pd.to_datetime(df['Time'])
+    
+    # Convert the game times to the local time zone (subtracting 3 hours)
+    df['Time'] = df['Time'] - pd.to_timedelta('3 hours')
 
-        # Rename the columns
-        df.rename(columns={
-            'FT_Odds_H': 'FT_Odd_H',
-            'FT_Odds_D': 'FT_Odd_D',
-            'FT_Odds_A': 'FT_Odd_A',
-            'FT_Odds_Over25': 'FT_Odd_Over25',
-            'FT_Odds_Under25': 'FT_Odd_Under25',
-            'Odds_BTTS_Yes': 'FT_Odd_BTTS_Yes',        
-            'ROUND': 'Rodada',
-        }, inplace=True)
-
-        # Apply the function to extract the round number and create a new column "Rodada_Num"
-        df["Rodada_Num"] = df["Rodada"].apply(extrair_numero_rodada)
-
-        return df
+    # Rename the columns
+    df.rename(columns={
+        'FT_Odds_H': 'FT_Odd_H',
+        'FT_Odds_D': 'FT_Odd_D',
+        'FT_Odds_A': 'FT_Odd_A',
+        'FT_Odds_Over25': 'FT_Odd_Over25',
+        'FT_Odds_Under25': 'FT_Odd_Under25',
+        'Odds_BTTS_Yes': 'FT_Odd_BTTS_Yes',        
+        'ROUND': 'Rodada',
+    }, inplace=True)
 
     # Função para extrair o número do texto "ROUND N"
     def extrair_numero_rodada(text):
@@ -50,9 +42,9 @@ def lay_zebra_page():
             return int(match.group())
         return None
 
-    # Call the load_base function to load the data
-    df = load_base()
-    
+    # Apply the function to extract the round number and create a new column "Rodada_Num"
+    df["Rodada_Num"] = df["Rodada"].apply(extrair_numero_rodada)
+
     # Filter matches with conditions
     layzebraht_df = df[
         (df["FT_Odd_H"] >= 1.01) & (df["FT_Odd_H"] <= 1.7) &
@@ -69,7 +61,7 @@ def lay_zebra_page():
     st.text("Apostar em Lay visitante, Odd máxima 6")
     st.dataframe(layzebraht_df)
 
-# Call the function to start the Streamlit app
+# Chamar a função para exibir a aplicação web
 lay_zebra_page()
 
 
