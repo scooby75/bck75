53cf0a0d50b9f4801f6a71d91e6cdcdb8ee3a868
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-24 10:18:37 -0300

    Update scalping.py

--- scalping.py
+++ scalping.py
@@ -2,36 +2,41 @@ import streamlit as st
 import pandas as pd
 
 def scalping_page():
-    # Carregar arquivos CSV via URL
+    # Definir URLs para os arquivos CSV
     url_jogosdodia = 'https://github.com/scooby75/bdfootball/blob/main/Jogos_do_Dia_FS.csv?raw=true'
     url_momento_gol_home = 'https://github.com/scooby75/bdfootball/blob/main/scalping_home.csv?raw=true'
     url_momento_gol_away = 'https://github.com/scooby75/bdfootball/blob/main/scalping_away.csv?raw=true'
 
-    jogosdodia = pd.read_csv(url_jogosdodia)
-    momento_gol_home = pd.read_csv(url_momento_gol_home)
-    momento_gol_away = pd.read_csv(url_momento_gol_away)
+    try:
+        # Carregar dados CSV
+        jogosdodia = pd.read_csv(url_jogosdodia)
+        momento_gol_home = pd.read_csv(url_momento_gol_home)
+        momento_gol_away = pd.read_csv(url_momento_gol_away)
 
-    # Filtrar jogos em que 0-15_mar e 0-15_sofri seja == 0 em ambos os DataFrames
-    jogos_filtrados_home = jogosdodia.merge(momento_gol_home, left_on='Home', right_on='Equipe')
-    jogos_filtrados_away = jogosdodia.merge(momento_gol_away, left_on='Away', right_on='Equipe')
+        # Lógica de mesclagem e filtragem de dados
+        jogos_filtrados_home = jogosdodia.merge(momento_gol_home, left_on='Home', right_on='Equipe')
+        jogos_filtrados_away = jogosdodia.merge(momento_gol_away, left_on='Away', right_on='Equipe')
+        jogos_filtrados = jogos_filtrados_home.merge(jogos_filtrados_away, on=['Date', 'Home', 'Away'], suffixes=('_home', '_away'))
 
-    jogos_filtrados = jogos_filtrados_home.merge(jogos_filtrados_away, on=['Date', 'Home', 'Away'], suffixes=('_home', '_away'))
+        # Filtrar jogos com critérios específicos
+        filtered_games = jogos_filtrados[
+            (jogos_filtrados['0_15_mar_home'] == 0) & (jogos_filtrados['0_15_sofri_home'] == 0) &
+            (jogos_filtrados['0_15_mar_away'] == 0) & (jogos_filtrados['0_15_sofri_away'] == 0)
+        ]
 
-    filtered_games = jogos_filtrados[
-        (jogos_filtrados['0_15_mar_home'] == 0) & (jogos_filtrados['0_15_sofri_home'] == 0) &
-        (jogos_filtrados['0_15_mar_away'] == 0) & (jogos_filtrados['0_15_sofri_away'] == 0)
-    ]
+        # Selecionar colunas relevantes e renomear
+        result_df = filtered_games[['Home', 'Away', 'FT_Odd_H_home', 'FT_Odd_A_home', 'FT_Odd_Over25_home']]
+        result_df.columns = ['Casa', 'Visitante', 'Odd_Casa', 'Odd_Visitante', 'Odd_MaisDe25']
 
-    # Selecionar colunas relevantes
-    result_df = filtered_games[['Home', 'Away', 'FT_Odd_H_home', 'FT_Odd_A_home', 'FT_Odd_Over25_home']]
+        # Streamlit App
+        st.subheader("Lay Over 25FT")
+        st.text("Apostar em Lay Over 25FT e fechar posição com 3% ou 5min de exposição.")
+        st.dataframe(result_df)
 
-    # Renomear colunas
-    result_df.columns = ['Home', 'Away', 'FT_Odd_H', 'FT_Odd_A', 'FT_Odd_Over25']
+    except Exception as e:
+        st.error("Ocorreu um erro: " + str(e))
 
-    # Streamlit App
-    st.title("Jogos Filtrados")
-    st.dataframe(result_df)
-
-# Call the function to start the Streamlit app
+# Chamar a função para iniciar o aplicativo Streamlit
 scalping_page()
+
 
