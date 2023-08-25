cd550ea030d8bfd06bb88057454b27a9c77fe37b
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-25 15:38:32 -0300

    Update bck_home.py

--- bck_home.py
+++ bck_home.py
@@ -19,29 +19,23 @@ def bck_home_page():
 
     # Filter by League
     all_leagues = "Todos"
-    selected_leagues = st.multiselect("Selecionar Liga(s)", [all_leagues] + list(bck_home_df['League'].unique()), default=all_leagues)
-    if all_leagues in selected_leagues:
-        selected_leagues.remove(all_leagues)
+    selected_leagues = st.multiselect("Selecionar Liga(s)", [all_leagues] + list(bck_home_df['League'].unique()), default=[all_leagues])
 
     # Filter by Season
     all_seasons = "Todos"
-    selected_seasons = st.multiselect("Selecionar Temporada(s)", [all_seasons] + list(bck_home_df['Season'].unique()), default=all_seasons)
-    if all_seasons in selected_seasons:
-        selected_seasons.remove(all_seasons)
+    selected_seasons = st.multiselect("Selecionar Temporada(s)", [all_seasons] + list(bck_home_df['Season'].unique()), default=[all_seasons])
 
     # Multiselect for Round
     all_rounds = "Todos"
-    selected_rounds = st.multiselect("Selecionar Rodada(s)", [all_rounds] + list(bck_home_df['Round'].unique()), default=all_rounds)
-    if all_rounds in selected_rounds:
-        selected_rounds.remove(all_rounds)
+    selected_rounds = st.multiselect("Selecionar Rodada(s)", [all_rounds] + list(bck_home_df['Round'].unique()), default=[all_rounds])
 
     # ... Add other filters ...
 
     # Apply filters
     filtered_df = bck_home_df[
-        (bck_home_df['League'].isin(selected_leagues) | (all_leagues in selected_leagues)) &
-        (bck_home_df['Season'].isin(selected_seasons) | (all_seasons in selected_seasons)) &
-        (bck_home_df['Round'].isin(selected_rounds) | (all_rounds in selected_rounds))
+        (bck_home_df['League'].isin(selected_leagues) if all_leagues not in selected_leagues else True) &
+        (bck_home_df['Season'].isin(selected_seasons) if all_seasons not in selected_seasons else True) &
+        (bck_home_df['Round'].isin(selected_rounds) if all_rounds not in selected_rounds else True)
         # Add more filtering conditions for other columns here...
     ]
 
