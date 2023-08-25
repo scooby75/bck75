7b415565c4dee7fbe7ca0afab84bc1d27bbb653f
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-25 06:41:55 -0300

    Update scalping.py

--- scalping.py
+++ scalping.py
@@ -13,9 +13,9 @@ def scalping_page():
         return
 
     # Definir URLs para os arquivos CSV
-    url_jogosdodia = 'https://github.com/scooby75/bdfootball/raw/main/Jogos_do_Dia_FS.csv'
-    url_momento_gol_home = 'https://github.com/scooby75/bdfootball/raw/main/scalping_home.csv'
-    url_momento_gol_away = 'https://github.com/scooby75/bdfootball/raw/main/scalping_away.csv'
+    url_jogosdodia = 'https://github.com/scooby75/bdfootball/blob/main/Jogos_do_Dia_FS.csv?raw=true'
+    url_momento_gol_home = 'https://github.com/scooby75/bdfootball/blob/main/scalping_home.csv?raw=true'
+    url_momento_gol_away = 'https://github.com/scooby75/bdfootball/blob/main/scalping_away.csv?raw=true'
 
     try:
         # Carregar dados CSV
@@ -23,14 +23,10 @@ def scalping_page():
         momento_gol_home = pd.read_csv(url_momento_gol_home)
         momento_gol_away = pd.read_csv(url_momento_gol_away)
 
-        # Adicionar a coluna 'ROUND' aos DataFrames momento_gol_home e momento_gol_away
-        momento_gol_home['ROUND'] = momento_gol_home['league'].str.extract('ROUND (\d+)')[0]
-        momento_gol_away['ROUND'] = momento_gol_away['league'].str.extract('ROUND (\d+)')[0]
-
         # Lógica de mesclagem e filtragem de dados
-        jogos_filtrados_home = jogosdodia.merge(momento_gol_home, left_on=['Home', 'ROUND'], right_on=['Equipe', 'ROUND'], how='left')
-        jogos_filtrados_away = jogosdodia.merge(momento_gol_away, left_on=['Away', 'ROUND'], right_on=['Equipe', 'ROUND'], how='left')
-        jogos_filtrados = jogos_filtrados_home.merge(jogos_filtrados_away, on=['Date', 'Home', 'Away', 'ROUND'], suffixes=('_home', '_away'))
+        jogos_filtrados_home = jogosdodia.merge(momento_gol_home, left_on='Home', right_on='Equipe')
+        jogos_filtrados_away = jogosdodia.merge(momento_gol_away, left_on='Away', right_on='Equipe')
+        jogos_filtrados = jogos_filtrados_home.merge(jogos_filtrados_away, on=['Date', 'Home', 'Away'], suffixes=('_home', '_away'))
 
         # Filtrar jogos com critérios específicos
         filtered_games = jogos_filtrados[
@@ -39,14 +35,8 @@ def scalping_page():
         ]
 
         # Selecionar colunas relevantes e renomear
-        result_df = filtered_games[['Home', 'Away', 'FT_Odd_H_home', 'FT_Odd_A_home', 'FT_Odd_Over25_home', 'ROUND']]
-        result_df.columns = ['Home', 'Away', 'FT_Odd_H', 'FT_Odd_A', 'FT_Odd_Over25', 'Rodada']
-
-        # Extraindo apenas os números da coluna 'Rodada'
-        result_df['Rodada'] = result_df['Rodada'].str.extract('(\d+)')
-
-        # Filtrar jogos com ROUND >= 5
-        result_df = result_df[result_df['Rodada'].astype(int) >= 3]
+        result_df = filtered_games[['Home', 'Away', 'FT_Odd_H_home', 'FT_Odd_A_home', 'FT_Odd_Over25_home']]
+        result_df.columns = ['Home', 'Away', 'FT_Odd_H', 'FT_Odd_A', 'FT_Odd_Over25']
 
         # Streamlit App
         st.subheader("Lay Over 25FT")
@@ -58,3 +48,5 @@ def scalping_page():
 
 # Chamar a função para iniciar o aplicativo Streamlit
 scalping_page()
+
+
