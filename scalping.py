8fc764d6c4e3493f2695cc473415006087defe67
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-25 05:33:06 -0300

    Update scalping.py

--- scalping.py
+++ scalping.py
@@ -49,7 +49,6 @@ def scalping_page():
         ]
 
         # Extrair número da rodada e filtrar com condição
-        filtered_games["Rodada_Num"] = filtered_games["ROUND"].apply(extrair_numero_rodada)
         filtered_games = filtered_games[filtered_games["Rodada_Num"] >= 5]
 
         # Selecionar colunas relevantes e renomear
