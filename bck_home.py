aeda87387c039a793c8677fc9f4a51291a85c403
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-27 12:53:00 -0300

    Update bck_home.py

--- bck_home.py
+++ bck_home.py
@@ -309,7 +309,7 @@ def bck_home_page():
             roi = round(roi, 2)
     
     # Exibir os resultados usando st.write()
-            st.write(f"Lucro/Prejuízo: {lucro_total} em {total_de_jogos} jogos")
+            st.write(f"Lucro/Prejuízo: {lucro_total} Und em {total_de_jogos} jogos")
             st.write(f"Yield: {roi}%")
         else:
     # Exibir mensagem de DataFrame vazio
@@ -486,7 +486,7 @@ def bck_home_page():
             roi = round(roi, 2)
     
     # Exibir os resultados usando st.write()
-            st.write(f"Lucro/Prejuízo: {lucro_total} em {total_de_jogos} jogos")
+            st.write(f"Lucro/Prejuízo: {lucro_total} Und em {total_de_jogos} jogos")
             st.write(f"Yield: {roi}%")
         else:
     # Exibir mensagem de DataFrame vazio
@@ -553,7 +553,7 @@ def bck_home_page():
             roi = round(roi, 2)
     
     # Exibir os resultados usando st.write()
-            st.write(f"Lucro/Prejuízo: {lucro_total} em {total_de_jogos} jogos")
+            st.write(f"Lucro/Prejuízo: {lucro_total} Und em {total_de_jogos} jogos")
             st.write(f"Yield: {roi}%")
         else:
     # Exibir mensagem de DataFrame vazio
@@ -622,7 +622,7 @@ def bck_home_page():
             roi = round(roi, 2)
     
     # Exibir os resultados usando st.write()
-            st.write(f"Lucro/Prejuízo: {lucro_total} em {total_de_jogos} jogos")
+            st.write(f"Lucro/Prejuízo: {lucro_total} Und em {total_de_jogos} jogos")
             st.write(f"Yield: {roi}%")
         else:
     # Exibir mensagem de DataFrame vazio
@@ -689,7 +689,7 @@ def bck_home_page():
             roi = round(roi, 2)
     
     # Exibir os resultados usando st.write()
-            st.write(f"Lucro/Prejuízo: {lucro_total} em {total_de_jogos} jogos")
+            st.write(f"Lucro/Prejuízo: {lucro_total} Und em {total_de_jogos} jogos")
             st.write(f"Yield: {roi}%")
         else:
     # Exibir mensagem de DataFrame vazio
@@ -758,7 +758,7 @@ def bck_home_page():
             roi = round(roi, 2)
     
     # Exibir os resultados usando st.write()
-            st.write(f"Lucro/Prejuízo: {lucro_total} em {total_de_jogos} jogos")
+            st.write(f"Lucro/Prejuízo: {lucro_total} Und em {total_de_jogos} jogos")
             st.write(f"Yield: {roi}%")
         else:
     # Exibir mensagem de DataFrame vazio
@@ -825,7 +825,7 @@ def bck_home_page():
             roi = round(roi, 2)
     
     # Exibir os resultados usando st.write()
-            st.write(f"Lucro/Prejuízo: {lucro_total} em {total_de_jogos} jogos")
+            st.write(f"Lucro/Prejuízo: {lucro_total} Und em {total_de_jogos} jogos")
             st.write(f"Yield: {roi}%")
         else:
     # Exibir mensagem de DataFrame vazio
@@ -894,7 +894,7 @@ def bck_home_page():
             roi = round(roi, 2)
     
     # Exibir os resultados usando st.write()
-            st.write(f"Lucro/Prejuízo: {lucro_total} em {total_de_jogos} jogos")
+            st.write(f"Lucro/Prejuízo: {lucro_total} Und em {total_de_jogos} jogos")
             st.write(f"Yield: {roi}%")
         else:
     # Exibir mensagem de DataFrame vazio
@@ -961,7 +961,7 @@ def bck_home_page():
             roi = round(roi, 2)
     
     # Exibir os resultados usando st.write()
-            st.write(f"Lucro/Prejuízo: {lucro_total} em {total_de_jogos} jogos")
+            st.write(f"Lucro/Prejuízo: {lucro_total} Und em {total_de_jogos} jogos")
             st.write(f"Yield: {roi}%")
         else:
     # Exibir mensagem de DataFrame vazio
@@ -1030,7 +1030,7 @@ def bck_home_page():
             roi = round(roi, 2)
     
     # Exibir os resultados usando st.write()
-            st.write(f"Lucro/Prejuízo: {lucro_total} UND em {total_de_jogos} jogos")
+            st.write(f"Lucro/Prejuízo: {lucro_total} Und em {total_de_jogos} jogos")
             st.write(f"Yield: {roi}%")
         else:
     # Exibir mensagem de DataFrame vazio
@@ -1097,7 +1097,7 @@ def bck_home_page():
             roi = round(roi, 2)
     
     # Exibir os resultados usando st.write()
-            st.write(f"Lucro/Prejuízo: {lucro_total} UND em {total_de_jogos} jogos")
+            st.write(f"Lucro/Prejuízo: {lucro_total} Und em {total_de_jogos} jogos")
             st.write(f"Yield: {roi}%")
         else:
     # Exibir mensagem de DataFrame vazio
@@ -1115,6 +1115,73 @@ def bck_home_page():
         st.line_chart(filtered_df, x='Date', y='Lucro_Acumulado_U45FT', use_container_width=True)
 
 #########################################################
+
+##### Calculo Win/Loss Lay 0x1 ####
+
+        # Create a new DataFrame for the "Lay 0x1" table
+        df_lay_01 = pd.DataFrame(columns=["Win", "Loss", "Odd Justa"])
+
+        # Calculate the number of "Win" and "Loss" occurrences
+        num_win = len(filtered_df[(filtered_df["FT_Goals_H"] != 0) | (filtered_df["FT_Goals_A"] != 1)])
+        num_loss = len(filtered_df[(filtered_df["FT_Goals_H"] == 0) & (filtered_df["FT_Goals_A"] == 1)])
+        total_games = num_win + num_loss
+
+        # Check if total_games is not zero before performing division
+        if total_games != 0:
+        # Calculate win and loss percentages
+            win_percentage = (num_win / total_games) * 100
+            loss_percentage = (num_loss / total_games) * 100
+        else:
+        # Handle the case when total_games is zero
+            win_percentage = 0
+            loss_percentage = 0
+
+        # Calculate the fair odds with 2 decimal places
+        if win_percentage != 0:
+            fair_odd = round(100 / win_percentage, 2)
+        else:
+        # Handle the case when win_percentage is zero
+            fair_odd = 0
+
+        #### Add the data to the "Over 45FT" table ####
+        df_lay_01.loc[0] = [f"{win_percentage:.2f}%", f"{loss_percentage:.2f}%", fair_odd]
+
+        # Display the "Lay 0x1" table
+        st.subheader("Lay 0x1")
+        st.dataframe(df_lay_01)
+
+    ###### Calculo Lucro/Prejuízo ####
+
+    # Verificar se o DataFrame não está vazio
+        if not filtered_df.empty:
+    # Somar os valores da coluna 'profit_home' para obter o lucro total
+            lucro_total = filtered_df['profit_Lay_0x1'].sum()
+            
+    # Calcular o ROI
+            total_de_jogos = len(filtered_df)
+            roi = (lucro_total / total_de_jogos) * 100
+    
+    # Arredondar os valores para duas casas decimais
+            lucro_total = round(lucro_total, 2)
+            roi = round(roi, 2)
+    
+    # Exibir os resultados usando st.write()
+            st.write(f"Lucro/Prejuízo: {lucro_total} Und em {total_de_jogos} jogos")
+            st.write(f"Yield: {roi}%")
+        else:
+    # Exibir mensagem de DataFrame vazio
+            st.write("Nenhum dado disponível. O DataFrame está vazio.")
+
+    ###### ADD Gráfico Lay 0x1 #####   
+
+    # Ordenar o dataframe pela coluna Date (caso não esteja ordenado)
+        filtered_df.sort_values(by='Date', inplace=True)
+
+    # Calcular o acumulado de capital ao longo do tempo (soma cumulativa da coluna Profit)
+        filtered_df['Lucro_Acumulado_Lay_01'] = filtered_df['profit_Lay_0x1'].cumsum()
+
+    # Criar o gráfico de linha com o acumulado de capital ao longo do tempo
+        st.line_chart(filtered_df, x='Date', y='Lucro_Acumulado_Lay_01', use_container_width=True)
 
    
 
