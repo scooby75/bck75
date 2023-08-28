6b024f2de722c4f37b9e09d6944f1c2ba5853bc2
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-28 06:06:41 -0300

    Update bck_league_home.py

--- bck_league_home.py
+++ bck_league_home.py
@@ -283,55 +283,54 @@ def bck_league_home_page():
     with tab2:
         #################################################
 
-                # Carregar os dados
+      # Carregar os dados
         @st.cache_data(ttl=28800.0)  # 24 horas em segundos
         def load_base():
             url = "https://github.com/scooby75/bdfootball/blob/main/BD_Geral.csv?raw=true"
             df = pd.read_csv(url)
             return df
-        
-        # Chamar a função para carregar os dados
+
+# Chamar a função para carregar os dados
         top_league_home_df = load_base()
 
-        # Filtros interativos
+# Filtros interativos
         st.header("Filtros")
 
-       all_leagues = "Todos"
-            selected_leagues = st.multiselect("Selecionar Liga(s)", [all_leagues] + list(top_league_home_df['League'].unique()), key="selected_leagues")
+        all_leagues = "Todos"
+        selected_leagues = st.multiselect("Selecionar Liga(s)", [all_leagues] + list(top_league_home_df['League'].unique()), key="selected_leagues")
 
-            all_rounds = "Todos"
-            selected_rounds = st.multiselect("Selecionar Rodada(s)", [all_rounds] + list(top_league_home_df['Round'].unique()), key="selected_rounds")
+        all_rounds = "Todos"
+        selected_rounds = st.multiselect("Selecionar Rodada(s)", [all_rounds] + list(top_league_home_df['Round'].unique()), key="selected_rounds")
 
-            all_seasons = "Todos"
-            selected_seasons = st.multiselect("Selecionar Temporada(s)", [all_seasons] + list(top_league_home_df['Season'].unique()), key="selected_seasons")
+        all_seasons = "Todos"
+        selected_seasons = st.multiselect("Selecionar Temporada(s)", [all_seasons] + list(top_league_home_df['Season'].unique()), key="selected_seasons")
 
-            home_teams = top_league_home_df['Home'].unique()  # Get unique teams from 'Home' column
-            selected_home = st.multiselect("Selecionar Mandante", home_teams, key="selected_home")
+        home_teams = top_league_home_df['Home'].unique()  # Get unique teams from 'Home' column
+        selected_home = st.multiselect("Selecionar Mandante", home_teams, key="selected_home")
 
-        # Filter for Odd_Home and Odd_Away range
+# Filter for Odd_Home and Odd_Away range
         with col2:
             odd_h_min = st.number_input("Odd_Home Mínimo", value=0.0, key="odd_h_min")
             odd_h_max = st.number_input("Odd_Home Máximo", value=10.0, key="odd_h_max")
-
+    
             odd_a_min = st.number_input("Odd_Away Mínimo", value=0.0, key="odd_a_min")
             odd_a_max = st.number_input("Odd_Away Máximo", value=10.0, key="odd_a_max")
-    
+
             odd_draw_min = st.number_input("Odd_Empate Mínimo", value=0.0, key="odd_draw_min")
             odd_draw_max = st.number_input("Odd_Empate Máximo", value=10.0, key="odd_draw_max")
 
-        # Filter for Over_05HT (HT_Odd_Over05) range and Over_25FT (FT_Odd_Over25)
+# Filter for Over_05HT (HT_Odd_Over05) range and Over_25FT (FT_Odd_Over25)
         with col3:
-            
             over_05ht_min = st.number_input("Over_05HT Mínimo", value=0.0, key="over_05ht_min")
             over_05ht_max = st.number_input("Over_05HT Máximo", value=10.0, key="over_05ht_max")
-
+    
             over_25ft_min = st.number_input("Over_25FT Mínimo", value=0.0, key="over_25ft_min")
             over_25ft_max = st.number_input("Over_25FT Máximo", value=10.0, key="over_25ft_max")
 
             btts_yes_min = st.number_input("BTTS_Yes Mínimo", value=0.0, key="btts_yes_min")
             btts_yes_max = st.number_input("BTTS_Yes Máximo", value=10.0, key="btts_yes_max")
 
-        # Apply filters
+# Apply filters
         filtered_df = top_league_home_df[
             (top_league_home_df['League'].isin(selected_leagues) if all_leagues not in selected_leagues else True) &
             (top_league_home_df['Season'].isin(selected_seasons) if all_seasons not in selected_seasons else True) &
@@ -351,11 +350,12 @@ def bck_league_home_page():
             (top_league_home_df['Odd_BTTS_Yes'] <= btts_yes_max)
         ]
 
-        selected_columns = [
+selected_columns = [
             "Date", "League", "Season", "Round", "Home", "Away",
             "FT_Odd_H", "FT_Odd_D", "FT_Odd_A", "HT_Odd_Over05", "FT_Odd_Over25", "Odd_BTTS_Yes", "Placar_HT", "Placar_FT", "profit_Lay_0x1",
         ]
         st.dataframe(filtered_df[selected_columns])
+
 
         
         ###########################################################3
