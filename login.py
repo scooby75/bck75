43c24f63e0f7bdd870c3668c3d52a8649acbde51
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-30 20:30:51 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,66 +1,45 @@
 # login.py
-import streamlit as st
-import datetime
+import sqlite3
 
-# Dictionary to track active users and their last activity time
-active_users = {}
+DATABASE_FILE = "user_sessions.db"
 
-# Session timeout in seconds (30 minutes)
-SESSION_TIMEOUT = 2 * 60
+def initialize_database():
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT PRIMARY KEY,
+            last_activity TIMESTAMP,
+            active INTEGER
+        )
+    """)
+    conn.commit()
+    conn.close()
 
-valid_users = {
-    "lsilveira": {"password": "senha123", "profile": 3},
-    "lamaral": {"password": "lamaral23", "profile": 1},
-    "blamim": {"password": "lamim23", "profile": 3},
-    "mrodrigues": {"password": "mrodrigues23", "profile": 3},
-    "user3": {"password": "password3", "profile": 3}
-}
+def login(username):
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+    cursor.execute("INSERT OR REPLACE INTO users (username, last_activity, active) VALUES (?, datetime('now'), 1)", (username,))
+    conn.commit()
+    conn.close()
 
-def login_page():
-    st.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660", width=240)
-    st.title("Football Data Analysis")
-    username = st.text_input("Usuário")
-    password = st.text_input("Senha", type="password")
+def logout(username):
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+    cursor.execute("UPDATE users SET active = 0 WHERE username = ?", (username,))
+    conn.commit()
+    conn.close()
 
-    login_button = st.button("Entrar")
-
-    if login_button:
-        if username in valid_users and valid_users[username]["password"] == password:
-            # Check if user is already logged in
-            if username in active_users:
-                st.error("Esse usuário já está logado em outro dispositivo.")
-                return
-
-            # Store user's session ID and last activity time
-            active_users[username] = datetime.datetime.now()
-            user_profile = valid_users[username]["profile"]
-            st.session_state.logged_in = True
-            st.session_state.username = username
-            st.session_state.user_profile = user_profile
-        else:
-            st.error("Credenciais inválidas.")
-
-def logout():
-    st.session_state.logged_in = False
-    st.session_state.pop("username", None)
-    st.session_state.pop("user_profile", None)
-
-# Check for session timeouts and remove inactive users
 def check_session_timeout():
-    current_time = datetime.datetime.now()
-    users_to_remove = []
-
-    for username, last_activity in active_users.items():
-        if (current_time - last_activity).total_seconds() > SESSION_TIMEOUT:
-            users_to_remove.append(username)
-
-    for username in users_to_remove:
-        active_users.pop(username)
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+    cursor.execute("UPDATE users SET active = 0 WHERE strftime('%s', datetime('now')) - strftime('%s', last_activity) > ?", (SESSION_TIMEOUT,))
+    conn.commit()
+    conn.close()
 
 def main():
-    # Chama a função check_session_timeout a cada 1 minuto
+    initialize_database()
     st.timer(interval=60, key="session_timeout_check")
-    
     login_page()
 
 if __name__ == "__main__":
