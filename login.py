7a3b4f3d234e6e3fb3b5fea196ec0ee9c5e51124
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-30 20:35:42 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,46 +1,60 @@
 # login.py
 import sqlite3
+import datetime
 
+# Database file
 DATABASE_FILE = "user_sessions.db"
+
+# Session timeout in seconds (30 minutes)
+SESSION_TIMEOUT = 30 * 60
 
 def initialize_database():
     conn = sqlite3.connect(DATABASE_FILE)
     cursor = conn.cursor()
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users (
+        CREATE TABLE IF NOT EXISTS users_credentials (
             username TEXT PRIMARY KEY,
-            last_activity TIMESTAMP,
-            active INTEGER
+            password TEXT
         )
     """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS active_users (
+            username TEXT PRIMARY KEY,
+            last_activity TIMESTAMP,
+            FOREIGN KEY (username) REFERENCES users_credentials(username)
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+def register_user(username, password):
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users_credentials (username, password) VALUES (?, ?)", (username, password))
     conn.commit()
     conn.close()
 
 def login(username):
     conn = sqlite3.connect(DATABASE_FILE)
     cursor = conn.cursor()
-    cursor.execute("INSERT OR REPLACE INTO users (username, last_activity, active) VALUES (?, datetime('now'), 1)", (username,))
+    cursor.execute("INSERT OR REPLACE INTO active_users (username, last_activity) VALUES (?, datetime('now'))", (username,))
     conn.commit()
     conn.close()
 
 def logout(username):
     conn = sqlite3.connect(DATABASE_FILE)
     cursor = conn.cursor()
-    cursor.execute("UPDATE users SET active = 0 WHERE username = ?", (username,))
+    cursor.execute("DELETE FROM active_users WHERE username = ?", (username,))
     conn.commit()
     conn.close()
 
 def check_session_timeout():
     conn = sqlite3.connect(DATABASE_FILE)
     cursor = conn.cursor()
-    cursor.execute("UPDATE users SET active = 0 WHERE strftime('%s', datetime('now')) - strftime('%s', last_activity) > ?", (SESSION_TIMEOUT,))
+    cursor.execute("DELETE FROM active_users WHERE strftime('%s', datetime('now')) - strftime('%s', last_activity) > ?", (SESSION_TIMEOUT,))
     conn.commit()
     conn.close()
 
-def main():
+if __name__ == "__main__":
     initialize_database()
     st.timer(interval=60, key="session_timeout_check")
-    login_page()
-
-if __name__ == "__main__":
-    main()
