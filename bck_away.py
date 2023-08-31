b86dc3ddde62b1db72a9296b63ac9c9e26f53032
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-31 20:12:25 -0300

    Update bck_away.py

--- bck_away.py
+++ bck_away.py
@@ -46,8 +46,8 @@ def bck_away_page():
             selected_away = st.multiselect("Selecionar Visitante", away_teams)
 
             # PPG_Away filter
-            min_rank_away = st.number_input("Rank Mínimo", min_value=1.0)
-            max_rank_away = st.number_input("Rank Máximo", max_value=50.0)
+            min_rank_away = st.number_input("Rank Mínimo (Away)", min_value=1.0, key="min_rank_away")
+            max_rank_away = st.number_input("Rank Máximo (Away)", max_value=50.0, key="max_rank_away")
 
         # Filter for Odd_Home and Odd_Away range
         with col2:
