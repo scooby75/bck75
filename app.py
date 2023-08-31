d32af73f96a12ed88642dfd93661689bd32882e3
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-31 20:50:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from login import login_page, logout, valid_users
+from login import login_page, logout
 from jogos import jogos_do_dia_page
 from cs import cs_page
 from predict import predict_page
@@ -13,19 +13,28 @@ from bck_league_home import bck_league_home_page
 from goleada import goleada_page
 from h2h import h2h_page
 
+from session_state import get_or_create_session_state
+
 def main():
-    if not logged_in_user:  # Verifica se o usuário está logado
+    # Obtém ou cria o estado da sessão
+    session_state = get_or_create_session_state()
+
+    if not hasattr(session_state, 'logged_in'):
+        session_state.logged_in = False
+
+    if not session_state.logged_in:
         login_page()
-    
     else:
-        user_profile = valid_users[logged_in_user]["profile"]  # Obtém o perfil do usuário logado
-        
+        # Initialize user_profile attribute if not present
+        if not hasattr(session_state, 'user_profile'):
+            session_state.user_profile = 1  # Initialize with a default value           
+
         # Barra lateral com imagem e informações
         st.sidebar.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660")
         st.sidebar.header("Football Data Analysis")
         
         # Mostra informações do usuário e botão de logout na barra lateral
-        st.sidebar.write(f"Logado como: {logged_in_user}")
+        st.sidebar.write(f"Logado como: {session_state.username}")
         if st.sidebar.button("Logout", key="logout_button"):
             logout()
         
@@ -33,6 +42,8 @@ def main():
         selected_tab = st.sidebar.selectbox("Selecione uma aba", ["Jogos do Dia", "Análise Home", "Análise Away", "Análise Liga", "Dutching CS", "HA", "H2H", "Lay Goleada", "Lay Zebra HT", "Lay Zebra FT", "Predict", "Scalping"])
 
         # Exibe o conteúdo da página selecionada, considerando as permissões do perfil
+        user_profile = session_state.user_profile  # Use session_state here
+        
         if selected_tab == "Jogos do Dia" and user_profile >= 1:
             jogos_do_dia_page()
         elif selected_tab == "Análise Home" and user_profile >= 3:
@@ -60,4 +71,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
