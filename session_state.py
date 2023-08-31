3dcef54644cdb6334f92ac1e66ef83b8a3de6904
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-08-31 04:33:28 -0300

    Update session_state.py

--- session_state.py
+++ session_state.py
@@ -4,3 +4,8 @@ class SessionState:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
+# Função para obter ou criar o estado da sessão
+def get_or_create_session_state():
+    if not hasattr(st, "session_state"):
+        st.session_state = SessionState()
+    return st.session_state
