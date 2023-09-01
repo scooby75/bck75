9ffec275b92f70efefc2911e4b8d303bfb18f434
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-09-01 12:14:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-# app.py
 import streamlit as st
-from login import login_page, perform_logout
+from login import login_page, logout
 from jogos import jogos_do_dia_page
 from cs import cs_page
 from predict import predict_page
@@ -13,56 +12,62 @@ from bck_away import bck_away_page
 from bck_league_home import bck_league_home_page
 from goleada import goleada_page
 from h2h import h2h_page
+
 from session_state import get_or_create_session_state
 
 def main():
     # Obtém ou cria o estado da sessão
     session_state = get_or_create_session_state()
 
-    if not hasattr(session_state, 'login_successful'):
-        session_state.login_successful = False  # Inicialize login_successful
-
     if not hasattr(session_state, 'logged_in'):
-        session_state.logged_in = False  # Inicialize logged_in
+        session_state.logged_in = False
 
     if not session_state.logged_in:
         login_page()
     else:
+        # Initialize user_profile attribute if not present
+        if not hasattr(session_state, 'user_profile'):
+            session_state.user_profile = 1  # Initialize with a default value           
+
         # Barra lateral com imagem e informações
         st.sidebar.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660")
         st.sidebar.header("Football Data Analysis")
-
+        
         # Mostra informações do usuário e botão de logout na barra lateral
         st.sidebar.write(f"Logado como: {session_state.username}")
         if st.sidebar.button("Logout", key="logout_button"):
-            perform_logout(session_state)
-
-        # Mapeia a seleção do usuário para as páginas
-        pages = {
-            "Jogos do Dia": (jogos_do_dia_page, 1),
-            "Análise Home": (bck_home_page, 3),
-            "Análise Away": (bck_away_page, 3),
-            "Análise Liga": (bck_league_home_page, 3),
-            "Dutching CS": (cs_page, 2),
-            "HA": (ha_025_page, 2),
-            "H2H": (h2h_page, 2),
-            "Lay Goleada": (goleada_page, 3),
-            "Lay Zebra HT": (lay_zebra_page, 2),
-            "Predict": (predict_page, 3),
-            "Lay Zebra FT": (zebra_ft_page, 2),
-            "Scalping": (scalping_page, 3)
-        }
-
+            logout()
+        
         # Caixa de seleção para diferentes páginas
-        selected_tab = st.sidebar.selectbox("Selecione uma aba", list(pages.keys()))
+        selected_tab = st.sidebar.selectbox("Selecione uma aba", ["Jogos do Dia", "Análise Home", "Análise Away", "Análise Liga", "Dutching CS", "HA", "H2H", "Lay Goleada", "Lay Zebra HT", "Lay Zebra FT", "Predict", "Scalping"])
 
         # Exibe o conteúdo da página selecionada, considerando as permissões do perfil
-        user_profile = session_state.user_profile
-
-        if selected_tab in pages:
-            page_function, required_profile = pages[selected_tab]
-            if user_profile is not None and user_profile >= required_profile:
-                page_function()
+        user_profile = session_state.user_profile  # Use session_state here
+        
+        if selected_tab == "Jogos do Dia" and user_profile >= 1:
+            jogos_do_dia_page()
+        elif selected_tab == "Análise Home" and user_profile >= 3:
+            bck_home_page()
+        elif selected_tab == "Análise Away" and user_profile >= 3:
+            bck_away_page()
+        elif selected_tab == "Análise Liga" and user_profile >= 3:
+            bck_league_home_page()
+        elif selected_tab == "Dutching CS" and user_profile >= 2:
+            cs_page()
+        elif selected_tab == "HA" and user_profile >= 2:
+            ha_025_page()
+        elif selected_tab == "H2H" and user_profile >= 2:
+            h2h_page()
+        elif selected_tab == "Lay Goleada" and user_profile == 3:
+            goleada_page()
+        elif selected_tab == "Lay Zebra HT" and user_profile >= 2:
+            lay_zebra_page()
+        elif selected_tab == "Predict" and user_profile == 3:
+            predict_page()
+        elif selected_tab == "Lay Zebra FT" and user_profile >= 2:
+            zebra_ft_page()
+        elif selected_tab == "Scalping" and user_profile == 3:
+            scalping_page()
 
 if __name__ == "__main__":
     main()
