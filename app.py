4782f305dc824f3b3c5089a055ee676bd4ef1026
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-09-02 06:10:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from bck_league_home import bck_league_home_page
 from goleada import goleada_page
 from h2h import h2h_page
 
+from session_state import get_or_create_session_state
 from session_state import SessionState
 
 def main():
