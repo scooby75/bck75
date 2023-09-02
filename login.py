9fd39e3d1ec90ecade9faeffaa12f9c6747781ec
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-09-02 08:09:43 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,72 +1,37 @@
 # login.py
 import streamlit as st
+import datetime
 from session_state import get_or_create_session_state
-from streamlit.server.server import Server
-import secure
 
-# Função para verificar se um usuário já está logado em outra sessão
-def is_user_already_logged_in(username):
-    all_sessions = secure.SecureCookie.load_all_sessions()
-    for session in all_sessions.values():
-        if "username" in session and session["username"] == username:
-            return True
-    return False
-
-def set_user_cookie(username):
-    session_id = Server.get_current()._session_info.session_id
-    st.experimental_set_query_params(username=username, session_id=session_id)
-
-def get_user_cookie():
-    session_id = Server.get_current()._session_info.session_id
-    query_params = st.experimental_get_query_params()
-    return query_params.get("username")
-
-# Lista de pares de usuário e senha válidos
-valid_users = [
-    {"username": "lsilveira", "password": "senha123"},
-    {"username": "usuario2", "password": "senha2"},
-    # Adicione mais pares de usuário e senha conforme necessário
-]
+valid_users = {
+    "lsilveira": {"password": "senha123", "profile": 3},
+    "lamaral": {"password": "lamaral23", "profile": 3},
+    "blamim": {"password": "lamim23", "profile": 3},
+    "mrodrigues": {"password": "mrodrigues23", "profile": 3},
+    "user3": {"password": "password3", "profile": 3}
+}
 
 def login_page():
-    session_state = get_or_create_session_state()
+    st.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660", width=240)
+    st.title("Football Data Analysis")
+    username = st.text_input("Usuário")
+    password = st.text_input("Senha", type="password")
 
-    if session_state.logged_in:
-        st.success(f"Você já está logado como {session_state.username}!")
-    else:
-        st.title("Login")
-        username = st.text_input("Nome de usuário")
-        password = st.text_input("Senha", type="password")
+    login_button = st.button("Entrar")
 
-        with st.form(key="login_form"):
-            st.write("Digite sua senha com segurança:")
-            st.write(f'<input type="password" name="password" value="{password}" placeholder="Senha">')
-            submitted = st.form_submit_button("Entrar")
-
-        if submitted:
-            # Verifique se o usuário já está logado em outra sessão
-            if is_user_already_logged_in(username):
-                st.error("Você já está logado em outra sessão. Faça logout antes de fazer login novamente.")
-                return
-
-            # Verifique as credenciais fornecidas com os pares válidos
-            if check_credentials(username, password):
-                session_state.logged_in = True
-                session_state.username = username
-                set_user_cookie(username)
-                st.success(f"Bem-vindo, {username}!")
-            else:
-                st.error("Nome de usuário ou senha incorretos.")
-
-def check_credentials(username, password):
-    for user in valid_users:
-        if username == user["username"] and password == user["password"]:
-            return True
-    return False
+    if login_button:
+        if username in valid_users and valid_users[username]["password"] == password:
+            session_state = get_or_create_session_state()
+            session_state.logged_in = True
+            session_state.username = username
+            session_state.login_time = datetime.datetime.now()
+            session_state.user_profile = valid_users[username]["profile"]  # Store user profile
+        else:
+            st.error("Credenciais inválidas.")
 
 def logout():
     session_state = get_or_create_session_state()
     session_state.logged_in = False
-    session_state.username = None
-    set_user_cookie("")  # Limpa o cookie de nome de usuário
-    st.info("Você foi desconectado com sucesso.")
+    session_state.pop("username", None)
+    session_state.pop("login_time", None)
+    session_state.pop("user_profile", None)  # Clear user profile on logout
