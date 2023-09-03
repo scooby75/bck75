62ebe238e1447019b1f2c48c480325ac0137a947
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-09-03 19:02:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,11 @@ from login import login_page, logout
 from jogos import jogos_do_dia_page
 from cs import cs_page
 from predict import predict_page
-from ha_025 import ha_025_page
-from lay_zebra import lay_zebra_page
-from zebra_ft import zebra_ft_page
-from scalping import scalping_page
 from bck_home import bck_home_page
 from bck_away import bck_away_page
 from bck_league_home import bck_league_home_page
-from goleada import goleada_page
-#from top5_home import top5_home_page
 from h2h import h2h_page
+from tips import tips_page
 
 from session_state import get_or_create_session_state
 from session_state import SessionState
@@ -46,7 +41,7 @@ def main():
             logout()
 
         # Caixa de seleção para diferentes páginas
-        selected_tab = st.sidebar.selectbox("Selecione uma aba", ["Jogos do Dia", "Análise Home", "Análise Away", "Análise Liga", "Dutching CS", "HA", "H2H", "Lay Goleada", "Lay Zebra HT", "Lay Zebra FT", "Predict", "Scalping"])
+        selected_tab = st.sidebar.selectbox("Selecione uma aba", ["Jogos do Dia", "Análise Home", "Análise Away", "Análise Liga", "Dutching CS",  "H2H", "Predict", "Tips"])
 
         # Exibe o conteúdo da página selecionada, considerando as permissões do perfil
         user_profile = session_state.user_profile  # Use session_state here
@@ -61,20 +56,13 @@ def main():
             bck_league_home_page()
         elif selected_tab == "Dutching CS" and user_profile >= 2:
             cs_page()
-        elif selected_tab == "HA" and user_profile >= 2:
-            ha_025_page()
         elif selected_tab == "H2H" and user_profile >= 2:
             h2h_page()
-        elif selected_tab == "Lay Goleada" and user_profile == 3:
-            goleada_page()
-        elif selected_tab == "Lay Zebra HT" and user_profile >= 2:
-            lay_zebra_page()
         elif selected_tab == "Predict" and user_profile == 3:
             predict_page()
-        elif selected_tab == "Lay Zebra FT" and user_profile >= 2:
-            zebra_ft_page()
-        elif selected_tab == "Scalping" and user_profile == 3:
-            scalping_page()
+        elif selected_tab == "Tips" and user_profile >= 1:
+            tips_page()
+        
        
 
 if __name__ == "__main__":
