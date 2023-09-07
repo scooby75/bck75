3fb9be921fe765fda67e110b65044d3512c550a4
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-09-07 12:41:56 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,4 +1,5 @@
 # login.py
+
 import streamlit as st
 import datetime
 from session_state import get_or_create_session_state
@@ -21,13 +22,13 @@ valid_users = {
 
 def login_page():
     st.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660", width=240)
-    st.title("Football Data Analysis")
+    st.title("Análise de Dados de Futebol")
     
-    # Add information message with a clickable link and "info-circle-fill" icon
+    # Adicione uma mensagem de informação com um link clicável e ícone "info-circle-fill"
     st.markdown('<i class="bi bi-info-circle-fill"></i> Informações sobre acesso, [clique aqui](https://t.me/Lyssandro).', unsafe_allow_html=True)
     
-    username = st.text_input("Usuário")
-    password = st.text_input("Senha", type="password")
+    username = st.text_input("Usuário", key="username_input")  # Chave única para o campo de entrada de usuário
+    password = st.text_input("Senha", type="password", key="password_input")  # Chave única para o campo de entrada de senha
 
     login_button = st.button("Entrar")
 
@@ -37,7 +38,7 @@ def login_page():
             session_state.logged_in = True
             session_state.username = username
             session_state.login_time = datetime.datetime.now()
-            session_state.user_profile = valid_users[username]["profile"]  # Store user profile
+            session_state.user_profile = valid_users[username]["profile"]  # Armazena o perfil do usuário
         else:
             st.error("Credenciais inválidas.")
 
@@ -46,8 +47,8 @@ def logout():
     session_state.logged_in = False
     session_state.pop("username", None)
     session_state.pop("login_time", None)
-    session_state.pop("user_profile", None)  # Clear user profile on logout
+    session_state.pop("user_profile", None)  # Limpa o perfil do usuário ao fazer logout
 
-# Add a Bootstrap icon to the login page
+# Execute a função para criar a página de login
 login_page()
 
