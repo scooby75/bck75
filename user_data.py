6d18298b5fbfc452eb64a2fb76bea8f47d2096c3
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-09-07 15:45:18 -0300

    Add files via upload

---
+++ user_data.py
@@ -0,0 +1,17 @@
+valid_users = {
+    "lsilveira": {"password": "senha123", "profile": 3},
+    "lamaral": {"password": "lamaral23", "profile": 1},
+    "blamim": {"password": "lamim23", "profile": 1},
+    "mrodrigues": {"password": "mrodrigues23", "profile": 3},
+    "alexbadcarrel": {"password": "Badminton76", "profile": 3},
+    "Ellwanger": {"password": "21ellwanger", "profile": 3}, 
+    "bulquinha": {"password": "evrr1111", "profile": 1}, 
+    "rafaelmax": {"password": "19cYqCqu!OpO", "profile": 1}, 
+    "Eanes": {"password": "Eanes@Analysis", "profile": 2}, 
+    "wagnercw": {"password": "G28M1V97@", "profile": 1}, 
+    "bjales": {"password": "kisso1cc", "profile": 3}, 
+    "DANIELSATOS": {"password": "140751@Ju", "profile": 3},
+    "GustavoMartins": {"password": "1985", "profile": 2}, 
+    "mmoren0": {"password": "mmoren23", "profile": 3}, 
+    "gblbet": {"password": "gblbet23", "profile": 2} 
+}
