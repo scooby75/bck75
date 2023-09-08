8bf656b93842adeb241e885396b3f13109a4bd2f
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-09-08 06:12:53 -0300

    Update login.py

--- login.py
+++ login.py
@@ -2,15 +2,17 @@
 
 import streamlit as st
 import datetime
-from session_state import get_or_create_session_state
 from user_data import valid_users  # Importe os dados dos usuários
 
-@st.experimental_singleton
-def initialize_session_state():
-    return get_or_create_session_state()
+# Dicionário global para armazenar informações de sessão
+session_state = {
+    "logged_in": False,
+    "username": None,
+    "login_time": None,
+    "user_profile": None,
+}
 
 def login_page():
-    session_state = initialize_session_state()  # Inicializa o session_state
     st.image("https://lifeisfootball22.files.wordpress.com/2021/09/data-2.png?w=660", width=240)
     st.title("Análise de Dados de Futebol")
     
@@ -24,20 +26,18 @@ def login_page():
 
     if login_button:
         if username in valid_users and valid_users[username]["password"] == password:
-            session_state.logged_in = True
-            session_state.username = username
-            session_state.login_time = datetime.datetime.now()
-            session_state.user_profile = valid_users[username]["profile"]  # Armazena o perfil do usuário
+            session_state["logged_in"] = True
+            session_state["username"] = username
+            session_state["login_time"] = datetime.datetime.now()
+            session_state["user_profile"] = valid_users[username]["profile"]  # Armazena o perfil do usuário
         else:
             st.error("Credenciais inválidas.")
 
 def logout():
-    session_state = initialize_session_state()  # Inicializa o session_state
-    session_state.logged_in = False
-    session_state.pop("username", None)
-    session_state.pop("login_time", None)
-    session_state.pop("user_profile", None)  # Limpa o perfil do usuário ao fazer logout
+    session_state["logged_in"] = False
+    session_state["username"] = None
+    session_state["login_time"] = None
+    session_state["user_profile"] = None  # Limpa o perfil do usuário ao fazer logout
 
 # Execute a função para criar a página de login
 login_page()
-
