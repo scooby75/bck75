3bb2a82d5455e975b8d0e309f61b90ee4844eae8
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-09-10 05:25:30 -0300

    Update user_data.py

--- user_data.py
+++ user_data.py
@@ -15,7 +15,7 @@ valid_users = {
     "Alison": {"password": "040712aJ@19", "profile": 3},
     "GustavoMartins": {"password": "1985", "profile": 2}, 
     "douglasgornimello": {"password": "feliz@!2023", "profile": 1}, 
-    "fabiofs": {"password": "'030791", "profile": 1}, 
+    "fabiofs": {"password": "030791", "profile": 1}, 
     "mmoren0": {"password": "mmoren23", "profile": 3}, 
     #"gblbet": {"password": "gblbet23", "profile": 2} 
 }
