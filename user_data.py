a3c23e56a28c3e5dc6f26c4f5980848b7db0bbe8
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-09-10 18:06:26 -0300

    Update user_data.py

--- user_data.py
+++ user_data.py
@@ -20,5 +20,6 @@ valid_users = {
     "wagner2662": {"password": "flamengo26624364", "profile": 3},
     "mmoren0": {"password": "mmoren23", "profile": 3}, 
     "willame_eduardo": {"password": "Will2015", "profile": 3},
+    "fagnerom": {"password": "120714", "profile": 2},
     "thomasgabriel": {"password": "thomas123", "profile": 1} 
 }
