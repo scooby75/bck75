92c229a770e4b900d3f29fa6b0fab032f1e88ac9
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-09-16 09:47:54 -0300

    Update tips.py

--- tips.py
+++ tips.py
@@ -22,7 +22,7 @@ def load_base():
     df = pd.read_csv(url)
 
     # Exclude rows where 'Home' or 'Away' contains specific substrings
-    substrings_to_exclude = ['U16', 'U17', 'U18', 'U19', 'U20', '21', 'U22', 'U23']
+    substrings_to exclude = ['U16', 'U17', 'U18', 'U19', 'U20', '21', 'U22', 'U23']
     mask = ~df['Home'].str.contains('|'.join(substrings_to_exclude), case=False) & \
            ~df['Away'].str.contains('|'.join(substrings_to_exclude), case=False)
     df = df[mask]
@@ -50,21 +50,21 @@ def tips_page():
         df = load_base()
 
         # ##### PÁGINA BCK HOME ######
-        tab0, tab1, tab2, tab3, tab4, tab5 = st.tabs(["HA", "Lay Goleada", "Lay Zebra HT", "Lay Zebra FT", "BTTS Sim", "Scalping"])
+        tab0, tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["HA", "Lay Goleada", "Lay Zebra HT", "Lay Zebra FT", "BTTS Sim", "Scalping", "LBB"])
 
         with tab0:
             # Use df aqui para a aba "HA"
             st.subheader("HA -0.25")
             st.text("Apostar em HA -0.25 casa, Odd mínima 1.40")
             ha_df = df[
-                (df["FT_Odd_H"] <= 1.70) &
+                (df["FT_Odd_H"] >= 1.40) & (df["FT_Odd_H"] <= 1.90) &                
                 (df["DC_1X"] <= 1.3) &
-                (df["PPG_Home"] >= 1.7) &
+                (df["PPG_Home"] >= 1.8) &
                 (df["Rodada"] >= 10)
             ]
             colunas_desejadas = ["Date", "Hora", "Liga", "Home", "Away"]
             ha_df = ha_df[colunas_desejadas]
-            st.dataframe(ha_df,width=800)
+            st.dataframe(ha_df, width=800)
 
             # Obter a data atual no formato desejado (por exemplo, "DD-MM-YYYY")
             data_atual = datetime.now().strftime("%d-%m-%Y")
@@ -74,7 +74,7 @@ def tips_page():
             st.download_button(
                 label="Baixar CSV",
                 data=csv_link,
-                file_name = f"handicap_asiatico_{data_atual}.csv",
+                file_name=f"handicap_asiatico_{data_atual}.csv",
                 key="handicap_asiatico_csv"
             )
 
@@ -85,7 +85,7 @@ def tips_page():
             eventos_raros_df = df[(df["FT_Odd_H"] >= 2) & (df["FT_Odd_H"] <= 5) & (df["FT_Odd_Over25"] >= 2.30) & (df["FT_Odd_BTTS_Yes"] <= 2) & (df["Rodada"] >= 10)]
             colunas_desejadas = ["Date", "Hora", "Liga", "Home", "Away"]
             eventos_raros_df = eventos_raros_df[colunas_desejadas]
-            st.dataframe(eventos_raros_df,width=800)
+            st.dataframe(eventos_raros_df, width=800)
 
             # Obter a data atual no formato desejado (por exemplo, "DD-MM-YYYY")
             data_atual = datetime.now().strftime("%d-%m-%Y")
@@ -103,7 +103,7 @@ def tips_page():
             st.text("Apostar em Lay Goleada Visitante, Odd máxima 30")
             eventos_raros2_df = df[(df["FT_Odd_A"] >= 2) & (df["FT_Odd_A"] <= 5) & (df["FT_Odd_Over25"] >= 2.30) & (df["FT_Odd_BTTS_Yes"] >= 2) & (df["Rodada"] >= 10)]
             eventos_raros2_df = eventos_raros2_df[colunas_desejadas]
-            st.dataframe(eventos_raros2_df,width=800)
+            st.dataframe(eventos_raros2_df, width=800)
 
             # Obter a data atual no formato desejado (por exemplo, "DD-MM-YYYY")
             data_atual = datetime.now().strftime("%d-%m-%Y")
@@ -129,7 +129,7 @@ def tips_page():
             ]
             colunas_desejadas = ["Date", "Hora", "Liga", "Home", "Away"]
             layzebraht_df = layzebraht_df[colunas_desejadas]
-            st.dataframe(layzebraht_df,width=800)
+            st.dataframe(layzebraht_df, width=800)
 
             # Obter a data atual no formato desejado (por exemplo, "DD-MM-YYYY")
             data_atual = datetime.now().strftime("%d-%m-%Y")
@@ -155,7 +155,7 @@ def tips_page():
             ]
             colunas_desejadas = ["Date", "Hora", "Liga", "Home", "Away"]
             layzebraft_df = layzebraft_df[colunas_desejadas]
-            st.dataframe(layzebraft_df,width=800)
+            st.dataframe(layzebraft_df, width=800)
 
             # Obter a data atual no formato desejado (por exemplo, "DD-MM-YYYY")
             data_atual = datetime.now().strftime("%d-%m-%Y")
@@ -182,7 +182,7 @@ def tips_page():
             ]
             colunas_desejadas = ["Date", "Hora", "Liga", "Home", "Away"]
             btts_yes_df = btts_yes_df[colunas_desejadas]
-            st.dataframe(btts_yes_df,width=800)
+            st.dataframe(btts_yes_df, width=800)
 
             # Obter a data atual no formato desejado (por exemplo, "DD-MM-YYYY")
             data_atual = datetime.now().strftime("%d-%m-%Y")
@@ -194,21 +194,6 @@ def tips_page():
                 data=csv_link,
                 file_name=f"btts_yes_{data_atual}.csv",
                 key="btts_yes_df_csv_4"
-            )
-
-            # Link para o LBB CSV
-            csv_link = "https://raw.githubusercontent.com/scooby75/bdfootball/main/btts.csv"
-
-            # Obter a data atual no formato desejado (por exemplo, "DD-MM-YYYY")
-            data_atual = datetime.now().strftime("%d-%m-%Y")
-
-            # Criar um link para download do CSV
-            csv_link = btts_yes_df.to_csv(index=False, encoding='utf-8-sig')
-            st.download_button(
-                label="Baixar CSV",
-                data=csv_link,
-                file_name=f"btts_yes_{data_atual}.csv",
-                key="btts_yes_df_csv_5"
             )
 
         with tab5:
@@ -245,7 +230,7 @@ def tips_page():
                 # Streamlit App
                 st.subheader("Lay Over 25FT")
                 st.text("Apostar em Lay Over 25FT e fechar posição com 3% ou 5min de exposição.")
-                st.dataframe(result_df,width=800)
+                st.dataframe(result_df, width=800)
 
                 # Obter a data atual no formato desejado (por exemplo, "DD-MM-YYYY")
                 data_atual = datetime.now().strftime("%d-%m-%Y")
@@ -262,5 +247,100 @@ def tips_page():
             except Exception as e:
                 st.error("Ocorreu um erro: " + str(e))
 
+        with tab6:
+
+             # Define separate load_base functions for each CSV file
+            @st.cache_data(ttl=21600.0)  # 06 hours in seconds
+            def load_lay_goleada_casa():
+                url = "https://raw.githubusercontent.com/scooby75/bdfootball/main/lay_goleda_casa.csv"
+                df_lay_goleada_casa = pd.read_csv(url)
+                return df_lay_goleada_casa
+
+            @st.cache_data(ttl=21600.0)  # 06 hours in seconds
+            def load_lay_goleada_visitante():
+                url = "https://raw.githubusercontent.com/scooby75/bdfootball/main/lay_goleada_visitante.csv"
+                df_lay_goleada_visitante = pd.read_csv(url)
+                return df_lay_goleada_visitante
+
+            @st.cache_data(ttl=21600.0)  # 06 hours in seconds
+            def load_lay_visitante_ht():
+                url = "https://raw.githubusercontent.com/scooby75/bdfootball/main/lay_zebra_ht.csv"
+                df_lay_visitante_ht = pd.read_csv(url)
+                return df_lay_visitante_ht
+
+            @st.cache_data(ttl=21600.0)  # 06 hours in seconds
+            def load_lay_visitante_ft():
+                url = "https://raw.githubusercontent.com/scooby75/bdfootball/main/lay_zebra_ft.csv"
+                df_lay_visitante_ft = pd.read_csv(url)
+                return df_lay_visitante_ft
+
+            @st.cache_data(ttl=21600.0)  # 06 hours in seconds
+            def load_btts():
+                url = "https://raw.githubusercontent.com/scooby75/bdfootball/main/btts.csv"
+                df_btts = pd.read_csv(url)
+                return df_btts
+
+            # Streamlit App
+            def tips_page():
+                st.subheader("Arquivos LBB - Lay Goleada Casa")
+                df_lay_goleada_casa = load_lay_goleada_casa()
+                st.dataframe(df_lay_goleada_casa.head(5), width=800)
+
+                st.subheader("Arquivos LBB - Lay Goleada Visitante")
+                df_lay_goleada_visitante = load_lay_goleada_visitante()
+                st.dataframe(df_lay_goleada_visitante.head(5), width=800)
+
+                st.subheader("Arquivos LBB - Lay Visitante HT")
+                df_lay_visitante_ht = load_lay_visitante_ht()
+                st.dataframe(df_lay_visitante_ht.head(5), width=800)
+
+                st.subheader("Arquivos LBB - Lay Visitante FT")
+                df_lay_visitante_ft = load_lay_visitante_ft()
+                st.dataframe(df_lay_visitante_ft.head(5), width=800)
+
+                st.subheader("Arquivos LBB - BTTS Sim")
+                df_btts = load_btts()
+                st.dataframe(df_btts.head(5), width=800)
+
+                # Obter a data atual no formato desejado (por exemplo, "DD-MM-YYYY")
+                data_atual = datetime.now().strftime("%d-%m-%Y")
+
+                # Criar um link para download do CSV para cada dataframe
+                st.download_button(
+                    label="Baixar Lay Goleada Casa",
+                    data=df_lay_goleada_casa.to_csv(index=False, encoding='utf-8-sig'),
+                    file_name=f"lay_goleada_casa_{data_atual}.csv",
+                    key="lay_goleada_casa"
+                )
+
+                st.download_button(
+                    label="Baixar Lay Goleada Visitante",
+                    data=df_lay_goleada_visitante.to_csv(index=False, encoding='utf-8-sig'),
+                    file_name=f"lay_goleada_visitante_{data_atual}.csv",
+                    key="lay_goleada_visitante"
+                )
+
+                st.download_button(
+                    label="Baixar Lay Visitante HT",
+                    data=df_lay_visitante_ht.to_csv(index=False, encoding='utf-8-sig'),
+                    file_name=f"lay_visitante_ht_{data_atual}.csv",
+                    key="lay_visitante_ht"
+                )
+
+                st.download_button(
+                    label="Baixar Lay Visitante FT",
+                    data=df_lay_visitante_ft.to_csv(index=False, encoding='utf-8-sig'),
+                    file_name=f"lay_visitante_ft_{data_atual}.csv",
+                    key="lay_visitante_ft"
+                )
+
+                st.download_button(
+                    label="Baixar BTTS Sim",
+                    data=df_btts.to_csv(index=False, encoding='utf-8-sig'),
+                    file_name=f"btts_{data_atual}.csv",
+                    key="btts"
+                )
+
+ 
 # Chama a função tips_page() no início do código para criar a página
 tips_page()
