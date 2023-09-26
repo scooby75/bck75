86c1afc234785561ef673637c64c7fc33ba72213
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-09-26 15:58:26 -0300

    Update user_data.py

--- user_data.py
+++ user_data.py
@@ -1,6 +1,6 @@
 valid_users = {
     "lsilveira": {"password": "senha123", "profile": 3},
-    #"lamaral": {"password": "lamaral23", "profile": 1},
+    "lamaral": {"password": "lamaral23", "profile": 3},
     "blamim": {"password": "lamim23", "profile": 3},
     "mrodrigues": {"password": "mrodrigues23", "profile": 3},
     "alexbadcarrel": {"password": "Badminton76", "profile": 3},
