472d79caa6061feaa6d48dbf428ab31fa3eef6a9
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-10-04 14:40:20 -0300

    Update stats_equipes.py

--- stats_equipes.py
+++ stats_equipes.py
@@ -31,7 +31,7 @@ def stats_equipes_page():
     liga_escolhida = st.selectbox("Escolha a liga:", df['League'].unique())
 
     # Solicite ao usuário o número de partidas a serem analisadas, limitado a 8
-    num_partidas = st.slider("Número de partidas a analisar:", min_value=1, max_value=min(8, len(df)), value=3)
+    num_partidas = st.slider("Número de partidas a analisar:", min_value=3, max_value=min(8, len(df)), value=3)
 
     # Filtre o DataFrame com base nas escolhas do usuário
     df_equipe_liga = df[(df['Home'] == equipe_escolhida) & (df['League'] == liga_escolhida)]
@@ -47,26 +47,32 @@ def stats_equipes_page():
 
     # Calcular as estatísticas das últimas N partidas selecionadas
     ultimas_partidas = df_equipe_liga.head(num_partidas).copy()
-
+    
     total_partidas = ultimas_partidas.shape[0]
-
-    # Usar .str.contains para contar as ocorrências de cada resultado
-    vitorias_FT = ultimas_partidas['Resultado_FT'].str.contains('Vitória').sum()
-    empates_FT = ultimas_partidas['Resultado_FT'].str.contains('Empate').sum()
-    derrotas_FT = ultimas_partidas['Resultado_FT'].str.contains('Derrota').sum()
-
-    vitorias_HT = ultimas_partidas['Resultado_HT'].str.contains('Vitória').sum()
-    empates_HT = ultimas_partidas['Resultado_HT'].str.contains('Empate').sum()
-    derrotas_HT = ultimas_partidas['Resultado_HT'].str.contains('Derrota').sum()
-
+    
+    # Mapear os valores nas colunas 'Resultado_FT' e 'Resultado_HT' para os resultados correspondentes
+    mapeamento_resultados = {'H': 'Vitória', 'D': 'Empate', 'A': 'Away'}
+    
+    ultimas_partidas['Resultado_FT'] = ultimas_partidas['Resultado_FT'].map(mapeamento_resultados)
+    ultimas_partidas['Resultado_HT'] = ultimas_partidas['Resultado_HT'].map(mapeamento_resultados)
+    
+    # Contar as ocorrências de cada resultado
+    vitorias_FT = ultimas_partidas['Resultado_FT'].eq('Vitória').sum()
+    empates_FT = ultimas_partidas['Resultado_FT'].eq('Empate').sum()
+    derrotas_FT = ultimas_partidas['Resultado_FT'].eq('Away').sum()  # Alterado de 'Derrota' para 'Away'
+    
+    vitorias_HT = ultimas_partidas['Resultado_HT'].eq('Vitória').sum()
+    empates_HT = ultimas_partidas['Resultado_HT'].eq('Empate').sum()
+    derrotas_HT = ultimas_partidas['Resultado_HT'].eq('Away').sum()  # Alterado de 'Derrota' para 'Away'
+    
     # Exibir as estatísticas
-    #st.subheader("Estatísticas:")
-    st.subheader("Resultados em FT:")
+    st.subheader("Estatísticas:")
+    st.write("Resultados em FT:")
     st.write(f"Vitórias: {vitorias_FT} ({(vitorias_FT / total_partidas * 100):.2f}%)")
     st.write(f"Empates: {empates_FT} ({(empates_FT / total_partidas * 100):.2f}%)")
     st.write(f"Derrotas: {derrotas_FT} ({(derrotas_FT / total_partidas * 100):.2f}%)")
-
-    st.subheader("Resultados em HT:")
+    
+    st.write("Resultados em HT:")
     st.write(f"Vitórias: {vitorias_HT} ({(vitorias_HT / total_partidas * 100):.2f}%)")
     st.write(f"Empates: {empates_HT} ({(empates_HT / total_partidas * 100):.2f}%)")
     st.write(f"Derrotas: {derrotas_HT} ({(derrotas_HT / total_partidas * 100):.2f}%)")
