bb45f5bba0def1c850a39987fb191a9a52961584
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-10-07 14:19:06 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -83,24 +83,24 @@ def cs_page():
     for index, row in resultado_df.iterrows():
         details1 = f"**Hora:** {row['Hora']}  |  **Home:** {row['Home']}  |  **Away:** {row['Away']}"
         details2 = f"**Odd Casa:** {row['FT_Odd_H']} |  **Odd Empate:** {row['FT_Odd_D']} |  **Odd Visitante:** {row['FT_Odd_A']}"
-        st.write(details1)
-        st.write(details2)
 
         # Criar um DataFrame temporário apenas com as probabilidades para o jogo atual
         prob_game_df = resultado_df[placares].iloc[[index]]
 
-        # Selecionar os 6 placares mais prováveis
-        top_placares = prob_game_df.T.nlargest(8, index)[index].index
+        # Verificar se a menor probabilidade em prob_game_df é >= 16%
+        if prob_game_df.min(axis=1).values[0] >= 16:
+            st.write(details1)
+            st.write(details2)
 
-        # Filtrar o DataFrame temporário para incluir apenas os 6 placares mais prováveis
-        prob_game_df = prob_game_df[top_placares]
+            # Selecionar os 6 placares mais prováveis
+            top_placares = prob_game_df.T.nlargest(8, index)[index].index
 
-        # Formatar e exibir a tabela
-        formatted_df = prob_game_df.applymap(lambda x: f"{x:.1f}%")
-        st.dataframe(formatted_df)
+            # Filtrar o DataFrame temporário para incluir apenas os 6 placares mais prováveis
+            prob_game_df = prob_game_df[top_placares]
+
+            # Formatar e exibir a tabela
+            formatted_df = prob_game_df.applymap(lambda x: f"{x:.1f}%")
+            st.dataframe(formatted_df)
 
 # Chamar a função para executar o aplicativo
 cs_page()
-
-
-
