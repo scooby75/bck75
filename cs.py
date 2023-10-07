cc67ea0945687bce657f8b0d02459c7c44bc2d2f
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-10-07 17:00:01 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -1,31 +1,43 @@
 import streamlit as st
 import pandas as pd
 from scipy.stats import poisson
+from streamlit import SessionState
 
 def cs_page():
+    # Inicializa o estado da sessão
+    session_state = SessionState()
+
+    # Defina o valor de user_profile após a criação da instância
+    session_state.user_profile = 2  # Ou qualquer outro valor desejado
+
+    # Verifica se o usuário tem permissão para acessar a página
+    if session_state.user_profile < 2:
+        st.error("Você não tem permissão para acessar esta página. Faça um upgrade do seu plano!!")
+        return
+
     # URL do arquivo CSV com os dados dos jogos
     url = "https://raw.githubusercontent.com/scooby75/bdfootball/main/Jogos_do_Dia_FS.csv"
 
     # Carregar os dados do arquivo CSV em um DataFrame
     df = pd.read_csv(url)
 
-    # Filtrar jogos com round maior ou igual a 10
-    df = df[df['Rodada'] >= 10]
+    # Filtrar jogos com rodada maior ou igual a 10
+    df_filtrado = df[df['Rodada'] >= 10]
 
-    # Filtrar jogos com home menor ou igual a 1.90
-    df = df[(df['FT_Odd_H'] >= 1.40) & (df['FT_Odd_H'] <= 2.4)]
+    # Filtrar jogos com cotação (odd) da equipe da casa entre 1.40 e 2.4
+    df_filtrado = df_filtrado[(df_filtrado['FT_Odd_H'] >= 1.40) & (df_filtrado['FT_Odd_H'] <= 2.4)]
 
-    # Filtrar jogos com home menor ou igual a 1.90
-    df = df[(df['FT_Odd_Under25'] <= 2)]
+    # Filtrar jogos com cotação (odd) de menos de 2.5 gols
+    df_filtrado = df_filtrado[df_filtrado['FT_Odd_Under25'] <= 2]
 
     # Placares para os quais você deseja calcular a probabilidade
-    placares = ['0x0', '1x0', '0x1', '1x1', '2x0', '0x2', '2x1', '1x2', '2x2', '3x0', '0x3', '3x2', '3x3', '4x0', '4x1', '4x2', '4x3', '4x4', '5x0', '5x1', '5x2', '5x3']
+    placares = ['0x0', '1x0', '0x1', '1x1', '2x0', '0x2', '2x1', '1x2', '2x2', '3x0', '3x1', '0x3', '3x2', '3x3', '1x3','2x3']
 
     # Lista para armazenar as linhas dos resultados
     linhas_resultados = []
 
-    # Iterar sobre os jogos e calcular as probabilidades para cada placar
-    for index, row in df.iterrows():
+    # Iterar sobre os jogos filtrados e calcular as probabilidades para cada placar
+    for index, row in df_filtrado.iterrows():
         # Calcular as médias de gols esperados para cada time e o total esperado
         lambda_home = row['XG_Home']
         lambda_away = row['XG_Away']
@@ -56,50 +68,35 @@ def cs_page():
         # Normalizar as probabilidades para que a soma seja 100%
         probabilidades = [prob / total_prob for prob in probabilidades]
 
-        # Criar uma linha para o resultado deste jogo
-        linha_resultado = {
-            'Date': row['Date'],
-            'Hora': row['Hora'],
-            'Liga': row['Liga'],
-            'Home': row['Home'],
-            'Away': row['Away'],
-            'FT_Odd_H': row['FT_Odd_H'],
-            'FT_Odd_D': row['FT_Odd_D'],
-            'FT_Odd_A': row['FT_Odd_A']
-        }
+        # Selecionar os 8 placares mais prováveis
+        top_placares = sorted(range(len(probabilidades)), key=lambda i: -probabilidades[i])[:8]
 
-        for i, placar in enumerate(placares):
-            linha_resultado[placar] = round(probabilidades[i] * 100, 2)
+        # Verificar se o primeiro placar mais provável é maior ou igual a 16%
+        if probabilidades[top_placares[0]] >= 16:
+            # Criar uma linha para o resultado deste jogo
+            linha_resultado = {
+                'Hora': row['Hora'],
+                'Home': row['Home'],
+                'Away': row['Away'],
+                'FT_Odd_H': row['FT_Odd_H'],
+                'FT_Odd_D': row['FT_Odd_D'],
+                'FT_Odd_A': row['FT_Odd_A']
+            }
 
-        linhas_resultados.append(linha_resultado)
+            for i in top_placares:
+                placar = placares[i]
+                prob = round(probabilidades[i] * 100, 2)
+                linha_resultado[placar] = prob
+
+            linhas_resultados.append(linha_resultado)
 
     # Criar um novo DataFrame com os resultados
     resultado_df = pd.DataFrame(linhas_resultados)
 
-    # Iniciar aplicativo Streamlit
-    st.subheader("Probabilidade de Placar")
-
-    # Loop para exibir os detalhes e a tabela
-    for index, row in resultado_df.iterrows():
-        details1 = f"**Hora:** {row['Hora']}  |  **Home:** {row['Home']}  |  **Away:** {row['Away']}"
-        details2 = f"**Odd Casa:** {row['FT_Odd_H']} |  **Odd Empate:** {row['FT_Odd_D']} |  **Odd Visitante:** {row['FT_Odd_A']}"
-        st.write(details1)
-        st.write(details2)
-
-        # Criar um DataFrame temporário apenas com as probabilidades para o jogo atual
-        prob_game_df = resultado_df[placares].iloc[[index]]
-
-        # Selecionar os 6 placares mais prováveis
-        top_placares = prob_game_df.T.nlargest(8, index)[index].index
-
-        # Filtrar o DataFrame temporário para incluir apenas os 6 placares mais prováveis
-        prob_game_df = prob_game_df[top_placares]
-
-        # Formatar e exibir a tabela
-        formatted_df = prob_game_df.applymap(lambda x: f"{x:.1f}%")
-        st.dataframe(formatted_df)
+    # Se houver jogos que atendam às condições originais, exibir o DataFrame de resultados
+    if not resultado_df.empty:
+        st.subheader("Probabilidade de Placar")
+        st.dataframe(resultado_df)
 
 # Chamar a função para executar o aplicativo
 cs_page()
-
-
