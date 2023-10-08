5c58bd5306c28ad194a1e4c2cb92b1cda76d8af6
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-10-08 06:44:24 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -92,20 +92,28 @@ def cs_page():
     # Iniciar aplicativo Streamlit
     st.subheader("Probabilidade de Placar")
 
-    # Loop para exibir os detalhes e a tabela apenas para jogos
-    # com os 6 placares mais prováveis em ordem decrescente
+    # Loop para exibir os detalhes e a tabela
     for index, row in resultado_df.iterrows():
-        # Criar um DataFrame temporário apenas com as informações relevantes
-        temp_df = pd.DataFrame(row[['Date', 'Hora', 'Liga', 'Home', 'Away', 'FT_Odd_H', 'FT_Odd_D', 'FT_Odd_A']]).transpose()
+        details1 = f"**Hora:** {row['Hora']}  |  **Home:** {row['Home']}  |  **Away:** {row['Away']}"
+        details2 = f"**Odd Casa:** {row['FT_Odd_H']} |  **Odd Empate:** {row['FT_Odd_D']} |  **Odd Visitante:** {row['FT_Odd_A']}"
+        st.write(details1)
+        st.write(details2)
 
-        # Adicionar as colunas de probabilidades para os 6 placares mais prováveis em ordem decrescente
-        top_placares = sorted(placares, key=lambda x: -row[x])[:6]
-        for placar in top_placares:
-            temp_df[placar] = row[placar]
+        # Criar um DataFrame temporário apenas com as probabilidades para o jogo atual
+        prob_game_df = resultado_df[placares].iloc[[index]]
 
-        # Verificar se o menor placar mais provável é >= 16%
-        if temp_df.iloc[0, -1] >= 16:
-            st.dataframe(temp_df)
+        # Selecionar os 6 placares mais prováveis
+        top_placares = prob_game_df.T.nlargest(8, index)[index].index
+
+        # Filtrar o DataFrame temporário para incluir apenas os 6 placares mais prováveis
+        prob_game_df = prob_game_df[top_placares]
+
+        # Formatar e exibir a tabela
+        formatted_df = prob_game_df.applymap(lambda x: f"{x:.1f}%")
+        st.dataframe(formatted_df)
 
 # Chamar a função para executar o aplicativo
 cs_page()
+
+
+
