fbcf982c0e3f93f77894772856caed698b349584
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-10-08 06:49:25 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -92,21 +92,22 @@ def cs_page():
     # Iniciar aplicativo Streamlit
     st.subheader("Probabilidade de Placar")
 
-    # Loop para exibir os detalhes e a tabela
+    # Loop para exibir os detalhes e a tabela apenas para jogos
+    # com os 8 placares mais prováveis em ordem decrescente
     for index, row in resultado_df.iterrows():
+        # Criar um DataFrame temporário apenas com as probabilidades para o jogo atual
+        prob_game_df = resultado_df[placares].iloc[[index]]
+
+        # Selecionar os 8 placares mais prováveis em ordem decrescente
+        top_placares = prob_game_df.T.nlargest(8).index
+
+        # Filtrar o DataFrame temporário para incluir apenas os 8 placares mais prováveis
+        prob_game_df = prob_game_df[top_placares]
+
         details1 = f"**Hora:** {row['Hora']}  |  **Home:** {row['Home']}  |  **Away:** {row['Away']}"
         details2 = f"**Odd Casa:** {row['FT_Odd_H']} |  **Odd Empate:** {row['FT_Odd_D']} |  **Odd Visitante:** {row['FT_Odd_A']}"
         st.write(details1)
         st.write(details2)
-
-        # Criar um DataFrame temporário apenas com as probabilidades para o jogo atual
-        prob_game_df = resultado_df[placares].iloc[[index]]
-
-        # Selecionar os 6 placares mais prováveis
-        top_placares = prob_game_df.T.nlargest(8, index)[index].index
-
-        # Filtrar o DataFrame temporário para incluir apenas os 6 placares mais prováveis
-        prob_game_df = prob_game_df[top_placares]
 
         # Formatar e exibir a tabela
         formatted_df = prob_game_df.applymap(lambda x: f"{x:.1f}%")
@@ -114,6 +115,3 @@ def cs_page():
 
 # Chamar a função para executar o aplicativo
 cs_page()
-
-
-
