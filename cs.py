0cd22184f5f8f751e67caac6c51f7034c4b6f6de
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-10-10 17:48:18 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -1,119 +1,94 @@
-import streamlit as st
 import pandas as pd
+import numpy as np
+import streamlit as st
 from scipy.stats import poisson
 
 from session_state import SessionState
 
 def cs_page():
-    # Initialize session state
+    # Inicializa o estado da sessão
     session_state = SessionState()
 
-    # Set the user_profile value after creating the instance
-    session_state.user_profile = 2  # Or any other desired value
+    # Defina o valor de user_profile após a criação da instância
+    session_state.user_profile = 2  # Ou qualquer outro valor desejado
 
-    # Check if the user has permission to access the page
+    # Verifica se o usuário tem permissão para acessar a página
     if session_state.user_profile < 2:
         st.error("Você não tem permissão para acessar esta página. Faça um upgrade do seu plano!!")
         return
 
-    # URL of the CSV file with game data
+    # URL do arquivo CSV com os dados dos jogos
     url = "https://raw.githubusercontent.com/scooby75/bdfootball/main/Jogos_do_Dia_FS.csv"
-
-    # Load data from the CSV file into a DataFrame
     df = pd.read_csv(url)
 
-    # Filter games with round greater than or equal to 10
-    df = df[df['Rodada'] >= 10]
+    # Definir os placares desejados
+    placares = [(i, j) for i in range(8) for j in range(8)]
 
-    # Filter games with home odds less than or equal to 1.90
-    df = df[(df['FT_Odd_H'] >= 1.80) & (df['FT_Odd_A'] >= 1.80) & (df['FT_Odd_Under25'] <= 2)]
+    # Filtrar partidas que não contenham as strings "U21", "U19", "U20", "U16", "U23" ou "U18" nas colunas "Home" ou "Away"
+    df = df[~df['Home'].str.contains(r'(U21|U19|U20|U16|U23|U18)', case=False) & ~df['Away'].str.contains(r'(U21|U19|U20|U16|U23|U18)', case=False)]
 
-    # Scores for which you want to calculate the probability
-    placares = ['1x0', '0x1', '1x1', '2x0', '0x2', '2x1', '1x2', '2x2', '3x0', '0x3', '3x1', '3x2', '3x3', '1x3', '2x3']
+    # Inicializar uma lista para armazenar as informações das partidas
+    partidas_info = []
 
-    # List to store the result rows
-    linhas_resultados = []
-
-    # Iterate over the games and calculate probabilities for each score
+    # Calcular as probabilidades para cada partida usando Poisson
     for index, row in df.iterrows():
-        # Calculate expected goal averages for each team and the total expected goals
-        lambda_home = row['XG_Home']
-        lambda_away = row['XG_Away']
-        lambda_total = row['Average Goals']
+        date = row['Date']
+        hora = row['Hora']
+        liga = row['Liga']
+        home_team = row['Home']
+        away_team = row['Away']
+        odd_casa = row['FT_Odd_H']
+        odd_empate = row['FT_Odd_D']
+        odd_visitante = row['FT_Odd_A']
 
-        # Calculate probabilities using the Poisson distribution
-        probabilidades = []
-        total_prob = 0  # Total probability for normalization
+        # Calcular as probabilidades de gols para cada equipe
+        prob_home = poisson.pmf(np.arange(0, 8), row['XG_Home'])
+        prob_away = poisson.pmf(np.arange(0, 8), row['XG_Away'])
 
-        for placar in placares:
-            placar_split = placar.split('x')
-            gols_home = int(placar_split[0])
-            gols_away = int(placar_split[1])
+        # Calcular a probabilidade de cada placar possível
+        probabilidade_partida = np.outer(prob_home, prob_away)
 
-            prob_home = poisson.pmf(gols_home, lambda_home)
-            prob_away = poisson.pmf(gols_away, lambda_away)
-            prob_total = poisson.pmf(gols_home + gols_away, lambda_total)
+        # Classificar os placares com base nas probabilidades
+        placares_classificados = sorted(
+            [(i, j, probabilidade_partida[i][j]) for i in range(8) for j in range(8)],
+            key=lambda x: x[2],
+            reverse=True
+        )
 
-            # Apply zero-inflation adjustment for "strange" scores
-            if (lambda_home < gols_home) or (lambda_away < gols_away):
-                prob_placar = prob_home * prob_away * prob_total * 1.50
-            else:
-                prob_placar = prob_home * prob_away * prob_total
+        # Calcular a probabilidade do placar 1
+        probabilidade_placar_1 = placares_classificados[0][2] * 100  # Em porcentagem
 
-            total_prob += prob_placar
-            probabilidades.append(prob_placar)
+        # Verificar se a probabilidade do placar 1 está entre 15% e 21%
+        if 15 <= probabilidade_placar_1 <= 21:
+            # Armazenar as informações da partida e probabilidades
+            partida_info = {
+                'Date': date,
+                'Hora': hora,
+                'Liga': liga,
+                'Home': home_team,
+                'Away': away_team,
+                'Odd Casa': odd_casa,
+                'Odd Empate': odd_empate,
+                'Odd Visitante': odd_visitante,
+            }
 
-        # Normalize probabilities so that the sum is 100%
-        probabilidades = [prob / total_prob for prob in probabilidades]
+            for idx, (i, j, probabilidade) in enumerate(placares_classificados[:8]):
+                probabilidade_percentual = round(probabilidade * 100, 2)  # Arredonda para 2 casas decimais e converte em porcentagem
+                partida_info[f'Prob {idx + 1}'] = f"{i}x{j} ({probabilidade_percentual}%)"
 
-        # Create a row for the result of this game
-        linha_resultado = {
-            'Date': row['Date'],
-            'Hora': row['Hora'],
-            'Liga': row['Liga'],
-            'Home': row['Home'],
-            'Away': row['Away'],
-            'FT_Odd_H': row['FT_Odd_H'],
-            'FT_Odd_D': row['FT_Odd_D'],
-            'FT_Odd_A': row['FT_Odd_A']
-        }
+            partidas_info.append(partida_info)
 
-        for i, placar in enumerate(placares):
-            linha_resultado[f'{placar} (Probabilidade)'] = f'{round(probabilidades[i] * 100, 2)}%'
+    # Criar um DataFrame com as informações das partidas
+    partidas_df = pd.DataFrame(partidas_info)
 
-        linhas_resultados.append(linha_resultado)
+    # Exibir a tabela com todas as informações usando st.dataframe
+    st.subheader("Dutching CS ")
+    st.dataframe(partidas_df)
 
-    # Create a new DataFrame with the results
-    resultado_df = pd.DataFrame(linhas_resultados)
+    # Exportar o DataFrame para um arquivo CSV
+    partidas_df.to_csv('dutching_cs.csv', index=False)
 
-    # Start the Streamlit application
-    st.subheader("Dutching CS")
+    st.write("Arquivo CSV gerado com sucesso.")
 
-    # Loop to display details and the table only for games with probability between 14% and 20%
-    for index, row in resultado_df.iterrows():
-        # Create a temporary DataFrame with probabilities for the current game only
-        prob_game_df = resultado_df[placares].iloc[[index]]
-
-        # Select the most likely scores in descending order of probability
-        top_scores = prob_game_df.iloc[0].nlargest(8)
-
-        # Check if the probability of the most likely score is between 14% and 20%
-        if (top_scores.max() >= 14.0) and (top_scores.max() <= 20.0):
-            details1 = f"**Hora:** {row['Hora']}  |  **Partida:** {row['Home']} vs {row['Away']}"
-            details2 = f"**Odd Casa:** {row['FT_Odd_H']} |  **Odd Empate:** {row['FT_Odd_D']} |  **Odd Visitante:** {row['FT_Odd_A']}"
-            st.write(details1)
-            st.write(details2)
-
-            # Transpose the DataFrame to display probabilities in columns
-            formatted_df = top_scores.to_frame(name='Probabilidade')
-            formatted_df = formatted_df.transpose()  # Transpose the DataFrame
-            formatted_df.columns = [f"{placar} (Probabilidade)" for placar in formatted_df.columns]
-
-            # Display score along with probability
-            scores_and_probs = formatted_df.columns.tolist()
-            st.write(scores_and_probs)
-
-            st.dataframe(formatted_df)
-
-# Call the function to run the Streamlit application
+# Chamar a função para executar o aplicativo
 cs_page()
