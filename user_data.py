1a9c839c595311a025af5f53432f4e8b6388a4a4
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-10-11 01:36:56 -0300

    Update user_data.py

--- user_data.py
+++ user_data.py
@@ -1,99 +1,50 @@
-import streamlit as st
-import pandas as pd
-from scipy.stats import poisson
-
-from session_state import SessionState
-
-def cs_page():
-    # Inicializa o estado da sessão
-    session_state = SessionState()
-
-    # Defina o valor de user_profile após a criação da instância
-    session_state.user_profile = 2  # Ou qualquer outro valor desejado
-
-    # Verifica se o usuário tem permissão para acessar a página
-    if session_state.user_profile < 2:
-        st.error("Você não tem permissão para acessar esta página. Faça um upgrade do seu plano!!")
-        return
-
-    # URL do arquivo CSV com os dados dos jogos
-    url = "https://raw.githubusercontent.com/scooby75/bdfootball/main/Jogos_do_Dia_FS.csv"
-
-    # Carregar os dados do arquivo CSV em um DataFrame
-    df = pd.read_csv(url)
-
-    # Filtrar jogos com round maior ou igual a 10
-    df = df[df['Rodada'] >= 10]
-
-    # Filtrar jogos com home menor ou igual a 1.90
-    df = df[(df['FT_Odd_H'] >= 1.40) & (df['FT_Odd_H'] <= 2.4)]
-
-    # Filtrar jogos com home menor ou igual a 1.90
-    df = df[(df['FT_Odd_Under25'] <= 2)]
-
-    # Placares para os quais você deseja calcular a probabilidade
-    placares = ['1x0', '0x1', '1x1', '2x0', '0x2', '2x1', '1x2', '2x2']
-
-    # DataFrame para armazenar os resultados de todos os jogos do dia
-    resultado_global = pd.DataFrame(columns=[
-        'Date', 'Hora', 'Pais', 'Liga', 'Home', 'Away', 'Odd Casa', 'Odd Empate', 'Odd Visitante'] + placares)
-
-    # Iterar sobre os jogos e calcular as probabilidades para cada placar
-    for index, row in df.iterrows():
-        # Calcular as médias de gols esperados para cada time e o total esperado
-        lambda_home = row['XG_Home']
-        lambda_away = row['XG_Away']
-        lambda_total = row['Average Goals']
-
-        # Calcular as probabilidades usando a distribuição de Poisson
-        probabilidades = []
-        total_prob = 0  # Total de probabilidade para normalização
-
-        for placar in placares:
-            placar_split = placar.split('x')
-            gols_home = int(placar_split[0])
-            gols_away = int(placar_split[1])
-
-            prob_home = poisson.pmf(gols_home, lambda_home)
-            prob_away = poisson.pmf(gols_away, lambda_away)
-            prob_total = poisson.pmf(gols_home + gols_away, lambda_total)
-
-            # Aplicar o ajuste de zero inflado para placares "estranhos"
-            if (lambda_home < gols_home) or (lambda_away < gols_away):
-                prob_placar = prob_home * prob_away * prob_total * 1.50
-            else:
-                prob_placar = prob_home * prob_away * prob_total
-
-            total_prob += prob_placar
-            probabilidades.append(prob_placar)
-
-        # Normalizar as probabilidades para que a soma seja 100%
-        probabilidades = [prob / total_prob for prob in probabilidades]
-
-        # Adicionar os resultados a um dicionário
-        resultado_temporario = {
-            'Date': row['Date'],
-            'Hora': row['Hora'],
-            'Pais': row['Pais'],
-            'Liga': row['Liga'],
-            'Home': row['Home'],
-            'Away': row['Away'],
-            'Odd Casa': row['FT_Odd_H'],
-            'Odd Empate': row['FT_Odd_D'],
-            'Odd Visitante': row['FT_Odd_A']
-        }
-
-        for i, placar in enumerate(placares):
-            resultado_temporario[placar] = round(probabilidades[i] * 100, 2)
-
-        # Adicionar os resultados ao DataFrame global
-        resultado_global = resultado_global.append(resultado_temporario, ignore_index=True)
-
-    # Iniciar aplicativo Streamlit
-    st.subheader("Dutching CS")
-
-    # Exibir o DataFrame com os resultados
-    st.write(resultado_global)
-
-# Chamar a função para executar o aplicativo
-cs_page()
+valid_users = {
+    "Lyssandro": {"password": "Fcl2fcl2#", "profile": 3},
+    "lamaral": {"password": "lamaral23", "profile": 3},
+    "blamim": {"password": "lamim23", "profile": 3},
+    #"mrodrigues": {"password": "mrodrigues23", "profile": 3},
+    "alexbadcarrel": {"password": "Badminton76", "profile": 3},
+    #"Ellwanger": {"password": "21ellwanger", "profile": 3}, 
+    #"bulquinha": {"password": "evrr1111", "profile": 1}, 
+    #"rafaelmax": {"password": "19cYqCqu!OpO", "profile": 1}, 
+    #"Eanes": {"password": "Eanes@Analysis", "profile": 2}, 
+    #"wagnercw": {"password": "G28M1V97@", "profile": 1}, 
+    #"bjales": {"password": "kisso1cc", "profile": 3}, 
+    #"DANIELSANTOS": {"password": "140751@Ju", "profile": 3},
+    #"estevaobayerl": {"password": "Test2320", "profile": 3},
+    #"Alison": {"password": "040712aJ@19", "profile": 3},
+    #"GustavoMartins": {"password": "1985", "profile": 2}, 
+    #"douglasgornimello": {"password": "feliz@!2023", "profile": 1}, 
+    #"fabiofs": {"password": "030791", "profile": 1},
+    #"gugafdk": {"password": "Gui280419#", "profile": 3},
+    #"wagner2662": {"password": "flamengo26624364", "profile": 3},
+    #"mmoren0": {"password": "mmoren23", "profile": 3}, 
+    #"willame_eduardo": {"password": "Will2015", "profile": 3},
+    "santanabet85": {"password": "santana85", "profile": 3},
+    #"fagnerom": {"password": "120714", "profile": 3},
+    "Mayconjp19": {"password": "210319", "profile": 3},
+    #"Edward": {"password": "Edward23", "profile": 3},
+    "suporteacampo": {"password": "analise2023", "profile": 3}, 
+    "bhctorres1982": {"password": "Rafael2015*", "profile": 3},
+    "Felipe Gonçalves": {"password": "felipesantos", "profile": 3},
+    "Marcel Verona": {"password": "Euro2023", "profile": 1},
+    "Borguezam": {"password": "91579877Luiz.", "profile": 1},
+    "Romeu": {"password": "301510", "profile": 2},
+    "lucrobr": {"password": "cnRt6374#", "profile": 2},
+    "Marcos morais": {"password": "06091992", "profile": 3},
+    "lucastrader6": {"password": "0@Lp4Q8Fz", "profile": 3},
+    #"HarleyLeal": {"password": "Galo1353@", "profile": 3},
+    #"thomasgabriel": {"password": "thomas123", "profile": 1},
+    "ACarlosFreitas": {"password": "198120@Ac", "profile": 3},
+    "JorgeSilva": {"password": "Teste123", "profile": 3},
+    "Wandersonvip": {"password": "Hillsong932", "profile": 3},
+    "RenanTL": {"password": "renan123", "profile": 3},
+    "Julio": {"password": "Abcd102030*", "profile": 3},
+    "Lucasbd": {"password": "Laybackbot", "profile": 1},
+    "regiscotrim": {"password": "Cida1212@", "profile": 3},
+    "maycon22": {"password": "Meg1309", "profile": 3},
+    "RBManzano": {"password": "Melina91", "profile": 1},
+    "daniel25": {"password": "Bi@253082", "profile": 2},
+    "franklinmg": {"password": "Emp@#123", "profile": 3},
+    "fellipinho": {"password": "110099", "profile": 3}
+}
