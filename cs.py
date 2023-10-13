0f92a05dc2a50b8ea2d72fb349f24af068764d3c
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-10-13 05:31:56 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -35,14 +35,82 @@ def cs_page():
     # Specify the score outcomes to calculate probabilities for
     placares = ['1x0', '0x1', '1x1', '2x0', '0x2', '2x1', '1x2', '2x2']
 
-    # List of dictionaries to store the results of all the day's matches
+    # Lista de dicionários para armazenar os resultados de todos os jogos do dia
     linhas_resultados = []
 
-    # Iterate over the matches and calculate probabilities for each score outcome
-    for index, row in df.iterrows:
-        # ... (rest of your code, calculating probabilities and storing results)
+    # Iterar sobre os jogos e calcular as probabilidades para cada placar
+    for index, row in df.iterrows():
+        # Calcular as médias de gols esperados para cada time e o total esperado
+        lambda_home = row['XG_Home']
+        lambda_away = row['XG_Away']
+        lambda_total = row['Average Goals']
 
-    # Continue with the rest of your code
+        # Calcular as probabilidades usando a distribuição de Poisson
+        probabilidades = []
+        total_prob = 0  # Total de probabilidade para normalização
 
-# Call the function to run the application
+        for placar in placares:
+            placar_split = placar.split('x')
+            gols_home = int(placar_split[0])
+            gols_away = int(placar_split[1])
+
+            prob_home = poisson.pmf(gols_home, lambda_home)
+            prob_away = poisson.pmf(gols_away, lambda_away)
+            prob_total = poisson.pmf(gols_home + gols_away, lambda_total)
+
+            # Aplicar o ajuste de zero inflado para placares "estranhos"
+            if (lambda_home < gols_home) or (lambda_away < gols_away):
+                prob_placar = prob_home * prob_away * prob_total * 1.50
+            else:
+                prob_placar = prob_home * prob_away * prob_total
+
+            total_prob += prob_placar
+            probabilidades.append(prob_placar)
+
+        # Normalizar as probabilidades para que a soma seja 100%
+        probabilidades = [prob / total_prob for prob in probabilidades]
+
+        # Criar um dicionário para o resultado deste jogo
+        linha_resultado = {
+            'Date': row['Date'],
+            'Hora': row['Hora'],
+            'Pais': row['Pais'],
+            'Liga': row['Liga'],
+            'Home': row['Home'],
+            'Away': row['Away'],
+            'Odd Casa': row['FT_Odd_H'],
+            'Odd Empate': row['FT_Odd_D'],
+            'Odd Visitante': row['FT_Odd_A']
+        }
+
+        for i, placar in enumerate(placares):
+            # Formatar a probabilidade com uma casa decimal e em formato de porcentagem
+            prob_formatada = f"{probabilidades[i] * 100:.1f}%"
+            linha_resultado[placar] = prob_formatada
+
+        # Adicionar o dicionário à lista
+        linhas_resultados.append(linha_resultado)
+
+    # Criar um DataFrame com os resultados
+    resultado_global = pd.DataFrame(linhas_resultados)
+
+    # Iniciar aplicativo Streamlit
+    st.subheader("Dutching CS")
+
+    # Exibir o DataFrame com os resultados
+    st.write(resultado_global)
+
+    # Obter a data atual no formato desejado
+    data_atual = datetime.now().strftime('%d/%m/%Y')
+
+    # Criar um link para download do CSV
+    csv_link_cs = resultado_global.to_csv(index=False, encoding='utf-8-sig')
+    st.download_button(
+        label="Baixar CSV",
+        data=csv_link_cs,
+        file_name=f"dutching_cs_{data_atual}.csv",
+        key="dutching_cs_csv"
+    )
+
+# Chamar a função para executar o aplicativo
 cs_page()
