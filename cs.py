d8b180154115ac771cfc4ed886c9c03c45c939b7
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-10-13 05:44:18 -0300

    Update cs.py

--- cs.py
+++ cs.py
@@ -1,38 +1,42 @@
 import streamlit as st
 import pandas as pd
+import io
 from scipy.stats import poisson
 from session_state import SessionState
-from datetime import datetime
-import io
+from datetime import datetime  # Importe datetime para obter a data atual
+
+from io import StringIO
+from io import BytesIO
+
 
 def cs_page():
-    # Initialize session state
+    # Inicializa o estado da sessão
     session_state = SessionState()
 
-    # Set the user profile value after creating the instance
-    session_state.user_profile = 2  # Or any other desired value
+    # Defina o valor de user_profile após a criação da instância
+    session_state.user_profile = 2  # Ou qualquer outro valor desejado
 
-    # Check if the user has permission to access the page
+    # Verifica se o usuário tem permissão para acessar a página
     if session_state.user_profile < 2:
-        st.error("You do not have permission to access this page. Please upgrade your plan!!")
+        st.error("Você não tem permissão para acessar esta página. Faça um upgrade do seu plano!!")
         return
 
-    # URL of the CSV file with match data
+    # URL do arquivo CSV com os dados dos jogos
     url = "https://raw.githubusercontent.com/scooby75/bdfootball/main/Jogos_do_Dia_FS.csv"
 
-    # Load data from the CSV file into a DataFrame
+    # Carregar os dados do arquivo CSV em um DataFrame
     df = pd.read_csv(url)
 
-    # Filter out games with 'Liga' containing 'Reserve' or 'Women'
-    df = df[~df['Liga'].str.contains('Reserve|Women', case=False)]
+    # Filtrar jogos com round maior ou igual a 10
+    df = df[df['Rodada'] >= 10]
 
-    # Define a list of substrings to check for in 'Home' and 'Away' columns
-    exclude_substrings = ['U16', 'U17', 'U18', 'U19', 'U20', 'U21']
+    # Filtrar jogos com home menor ou igual a 1.90
+    df = df[(df['FT_Odd_H'] >= 1.80) & (df['FT_Odd_H'] <= 2.4)]
 
-    # Filter out games with 'Home' or 'Away' containing any of the specified substrings
-    df = df[~df['Home'].str.contains('|'.join(exclude_substrings), case=False) & ~df['Away'].str.contains('|'.join(exclude_substrings), case=False)]
+    # Filtrar jogos com home menor ou igual a 1.90
+    df = df[(df['FT_Odd_Under25'] <= 2)]
 
-    # Specify the score outcomes to calculate probabilities for
+    # Placares para os quais você deseja calcular a probabilidade
     placares = ['1x0', '0x1', '1x1', '2x0', '0x2', '2x1', '1x2', '2x2']
 
     # Lista de dicionários para armazenar os resultados de todos os jogos do dia
