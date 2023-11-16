fbefba602b9c9b9eae6985b8fef73574d20dfdf2
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-11-16 03:51:01 -0300

    Update tips.py

--- tips.py
+++ tips.py
@@ -321,9 +321,9 @@ def tips_page():
                     (jogos_filtrados_home['Odds_Away_Win'].between(2, 10)) &
                     (jogos_filtrados_home['Under35 Average'].between(85, 100)) &
                     (jogos_filtrados_home['Home Team'] == jogos_filtrados_home['Equipe']) &
-                    (jogos_filtrados_away['Away Team'] == jogos_filtrados_away['Equipe']) &
-                    (jogos_filtrados_home['31_45_mar'] <= 1) &
-                    (jogos_filtrados_away['31_45_mar'] <= 1)
+                    (jogos_filtrados_away['Away Team'] == jogos_filtrados_away['Equipe']) 
+                    #(jogos_filtrados_home['31_45_mar'] <= 1) &
+                    #(jogos_filtrados_away['31_45_mar'] <= 1)
                 )
 
                 jogos_filtrados_home = jogos_filtrados_home[condicoes_filtragem]
