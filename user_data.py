7afdc4ee02ee338bc03c54e13534aca2d7d8ab3d
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-11-18 04:36:01 -0300

    Update user_data.py

--- user_data.py
+++ user_data.py
@@ -49,6 +49,6 @@ valid_users = {
     #"MatheusFujii": {"password": "520044", "profile": 3},
     #"Chami1414": {"password": "Eljunquitokm23", "profile": 3},
     #"cleberssaba": {"password": "POsitivo80@", "profile": 3},
-    "LuisFreire50": {"password": "LA2023l+++", "profile": 3},
+    #"LuisFreire50": {"password": "LA2023l+++", "profile": 3},
     #"fellipinho": {"password": "110099", "profile": 3}
 }
