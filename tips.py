252047fd3ad94970136aa6f0ba558c702b5fafa1
scooby75 <45230721+scooby75@users.noreply.github.com>
2023-11-18 15:34:17 -0300

    Update tips.py

--- tips.py
+++ tips.py
@@ -322,8 +322,8 @@ def tips_page():
                     (jogosdodia['League'] != 'UEFA U21 Championship Qualification') &
                     (jogosdodia['Odds_Home_Win'].between(2, 10)) &
                     (jogosdodia['Odds_Away_Win'].between(2, 10)) &
-                    (jogosdodia['Over25 Average'].between(0, 25)) &
-                    (jogosdodia['Over35 Average'].between(0, 5))
+                    (jogosdodia['Over25 Average'].between(1, 25)) &
+                    (jogosdodia['Over35 Average'].between(1, 5))
                     #(jogos_filtrados_home['Home Team'] == jogos_filtrados_home['Equipe']) &
                     #(jogos_filtrados_away['Away Team'] == jogos_filtrados_away['Equipe']) 
                     #(jogos_filtrados_home['31_45_mar'] <= 1) &
